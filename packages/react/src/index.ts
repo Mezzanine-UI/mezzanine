4a1cfd5658e84535c719096d9a2c8e7c2885f044
Jay Chen <gcobs135102@gmail.com>
2020-12-31 14:52:15 +0800

    fix(react): re-export all things to root index.ts

---
+++ packages/react/src/index.ts
@@ -0,0 +1,26 @@
+export { cx } from './utils/cx';
+
+export {
+  ButtonColor,
+  ButtonComponent,
+  ButtonSize,
+  ButtonVariant,
+  ButtonProps,
+  default as Button,
+  IconButtonProps,
+  IconButton,
+} from './Button';
+export {
+  IconColor,
+  IconProps,
+  default as Icon,
+} from './Icon';
+export {
+  TypographyAlign,
+  TypographyColor,
+  TypographyComponent,
+  TypographyDisplay,
+  TypographyVariant,
+  TypographyProps,
+  default as Typography,
+} from './Typography';
