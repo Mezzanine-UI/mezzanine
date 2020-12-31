ec22668c84645243ea21d25c8fef4e9f3ff5acd9
Jay Chen <gcobs135102@gmail.com>
2020-12-31 12:27:13 +0800

    feat(icons): initial package and add some icons
    check-circle-filled
    check
    chevron-down
    chevron-up
    exclamation-circle-filled
    eye-slash
    eye
    info-circle-filled
    plus
    search
    spinner
    times-circle-filled
    times

---
+++ packages/icons/src/index.ts
@@ -0,0 +1,18 @@
+export { IconDefinition } from './typings';
+
+/**
+ * icons
+ */
+export { CheckIcon } from './check';
+export { CheckCircleFilledIcon } from './check-circle-filled';
+export { ChevronUpIcon } from './chevron-up';
+export { ChevronDownIcon } from './chevron-down';
+export { ExclamationCircleFilledIcon } from './exclamation-circle-filled';
+export { EyeIcon } from './eye';
+export { EyeSlashIcon } from './eye-slash';
+export { InfoCircleFilledIcon } from './info-circle-filled';
+export { PlusIcon } from './plus';
+export { SearchIcon } from './search';
+export { SpinnerIcon } from './spinner';
+export { TimesIcon } from './times';
+export { TimesCircleFilledIcon } from './times-circle-filled';
