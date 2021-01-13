f46fdb39c58ad8fdb393175e270a6d9b996bb702
Ariel Chen <arielclimbing@gmail.com>
2021-01-13 15:25:10 +0800

    refactor(react/input): removed @storybook/addon-knobs

--- packages/react/src/Input/Input.stories.tsx
+++ packages/react/src/Input/Input.stories.tsx
@@ -1,5 +1,4 @@
 import { PlusIcon, SearchIcon } from '@mezzanine-ui/icons';
-import { boolean } from '@storybook/addon-knobs';
 import Input from '.';
 import Icon from '../Icon';
 
@@ -7,199 +6,187 @@ export default {
   title: 'Basic/Input',
 };
 
-export const Basic = () => {
-  const error = boolean('error', true);
+export const Basic = () => (
+  <div
+    style={{
+      display: 'inline-grid',
+      gridTemplateColumns: 'repeat(3, 200px)',
+      gridTemplateRows: '60px',
+      gap: '16px',
+      alignItems: 'center',
+    }}
+  >
+    <Input
+      placeholder="請輸入文字"
+    />
+    <Input
+      placeholder="請輸入文字"
+      disabled
+    />
+    <Input
+      placeholder="請輸入文字"
+      error
+    />
+    <Input
+      placeholder="請輸入文字"
+      value="Example"
+      readOnly
+    />
+  </div>
+);
 
-  return (
-    <div
-      style={{
-        display: 'inline-grid',
-        gridTemplateColumns: 'repeat(3, 200px)',
-        gridTemplateRows: '60px',
-        gap: '16px',
-        alignItems: 'center',
-      }}
-    >
-      <Input
-        placeholder="請輸入文字"
-      />
-      <Input
-        placeholder="請輸入文字"
-        disabled
-      />
-      <Input
-        placeholder="請輸入文字"
-        error={error}
-      />
-      <Input
-        placeholder="請輸入文字"
-        value="Example"
-        readOnly
-      />
-    </div>
-  );
-};
+export const Sizes = () => (
+  <div
+    style={{
+      display: 'inline-grid',
+      gridTemplateColumns: 'repeat(3, 200px)',
+      gridTemplateRows: '60px',
+      gap: '16px',
+      alignItems: 'center',
+    }}
+  >
+    <Input
+      placeholder="請輸入文字"
+      size="small"
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="medium"
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="large"
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="small"
+      disabled
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="medium"
+      disabled
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="large"
+      disabled
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="small"
+      error
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="medium"
+      error
+    />
+    <Input
+      placeholder="請輸入文字"
+      size="large"
+      error
+    />
+  </div>
+);
 
-export const Sizes = () => {
-  const error = boolean('error', true);
-
-  return (
-    <div
-      style={{
-        display: 'inline-grid',
-        gridTemplateColumns: 'repeat(3, 200px)',
-        gridTemplateRows: '60px',
-        gap: '16px',
-        alignItems: 'center',
-      }}
-    >
-      <Input
-        placeholder="請輸入文字"
-        size="small"
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="medium"
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="large"
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="small"
-        disabled
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="medium"
-        disabled
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="large"
-        disabled
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="small"
-        error={error}
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="medium"
-        error={error}
-      />
-      <Input
-        placeholder="請輸入文字"
-        size="large"
-        error={error}
-      />
-    </div>
-  );
-};
-
-export const WithIcons = () => {
-  const error = boolean('error', true);
-
-  return (
-    <div
-      style={{
-        display: 'inline-grid',
-        gridTemplateColumns: 'repeat(3, 200px)',
-        gridTemplateRows: '60px',
-        gap: '16px',
-        alignItems: 'center',
-      }}
-    >
-      <Input
-        placeholder="請輸入文字"
-      />
-      <Input
-        placeholder="請輸入文字"
-        iconEnd={<Icon icon={PlusIcon} />}
-        disabled
-      />
-      <Input
-        placeholder="請輸入文字"
-        iconStart={<Icon icon={PlusIcon} />}
-        error={error}
-      />
-      <Input
-        placeholder="請輸入文字"
-        iconEnd={<Icon icon={PlusIcon} />}
-      />
-      <Input
-        placeholder="請輸入文字"
-        iconStart={<Icon icon={PlusIcon} />}
-      />
-      <Input
-        placeholder="請輸入文字"
-        iconStart={<Icon icon={PlusIcon} />}
-      />
-      <Input
-        placeholder="輸入金額"
-        textStart="$"
-        textEnd="TWD"
-        size="large"
-        numberOnly
-      />
-      <Input
-        placeholder="輸入金額"
-        textStart="$"
-        textEnd="TWD"
-        numberOnly
-      />
-      <Input
-        placeholder="輸入金額"
-        textStart="$"
-        textEnd="TWD"
-        size="small"
-        numberOnly
-      />
-      <Input
-        placeholder="輸入金額"
-        iconStart={<Icon icon={PlusIcon} />}
-        textEnd="價格"
-        size="large"
-        numberOnly
-      />
-      <Input
-        placeholder="輸入金額"
-        textStart="$"
-        textEnd="TWD"
-        numberOnly
-      />
-      <Input
-        placeholder="輸入金額"
-        textStart="$"
-        textEnd="TWD"
-        size="small"
-        numberOnly
-      />
-      <Input
-        alphabetOnly
-        maxLength={20}
-        placeholder="搜尋..."
-        iconStart={<Icon icon={SearchIcon} />}
-        size="large"
-        clearable
-      />
-      <Input
-        alphabetOnly
-        maxLength={20}
-        placeholder="搜尋..."
-        iconStart={<Icon icon={SearchIcon} />}
-        clearable
-      />
-      <Input
-        alphabetOnly
-        maxLength={20}
-        placeholder="搜尋..."
-        iconStart={<Icon icon={SearchIcon} />}
-        size="small"
-        clearable
-      />
-    </div>
-  );
-};
+export const WithIcons = () => (
+  <div
+    style={{
+      display: 'inline-grid',
+      gridTemplateColumns: 'repeat(3, 200px)',
+      gridTemplateRows: '60px',
+      gap: '16px',
+      alignItems: 'center',
+    }}
+  >
+    <Input
+      placeholder="請輸入文字"
+    />
+    <Input
+      placeholder="請輸入文字"
+      iconEnd={<Icon icon={PlusIcon} />}
+      disabled
+    />
+    <Input
+      placeholder="請輸入文字"
+      iconStart={<Icon icon={PlusIcon} />}
+      error
+    />
+    <Input
+      placeholder="請輸入文字"
+      iconEnd={<Icon icon={PlusIcon} />}
+    />
+    <Input
+      placeholder="請輸入文字"
+      iconStart={<Icon icon={PlusIcon} />}
+    />
+    <Input
+      placeholder="請輸入文字"
+      iconStart={<Icon icon={PlusIcon} />}
+    />
+    <Input
+      placeholder="輸入金額"
+      textStart="$"
+      textEnd="TWD"
+      size="large"
+      numberOnly
+    />
+    <Input
+      placeholder="輸入金額"
+      textStart="$"
+      textEnd="TWD"
+      numberOnly
+    />
+    <Input
+      placeholder="輸入金額"
+      textStart="$"
+      textEnd="TWD"
+      size="small"
+      numberOnly
+    />
+    <Input
+      placeholder="輸入金額"
+      iconStart={<Icon icon={PlusIcon} />}
+      textEnd="價格"
+      size="large"
+      numberOnly
+    />
+    <Input
+      placeholder="輸入金額"
+      textStart="$"
+      textEnd="TWD"
+      numberOnly
+    />
+    <Input
+      placeholder="輸入金額"
+      textStart="$"
+      textEnd="TWD"
+      size="small"
+      numberOnly
+    />
+    <Input
+      alphabetOnly
+      maxLength={20}
+      placeholder="搜尋..."
+      iconStart={<Icon icon={SearchIcon} />}
+      size="large"
+      clearable
+    />
+    <Input
+      alphabetOnly
+      maxLength={20}
+      placeholder="搜尋..."
+      iconStart={<Icon icon={SearchIcon} />}
+      clearable
+    />
+    <Input
+      alphabetOnly
+      maxLength={20}
+      placeholder="搜尋..."
+      iconStart={<Icon icon={SearchIcon} />}
+      size="small"
+      clearable
+    />
+  </div>
+);
--- packages/react/src/Textarea/Textarea.stories.tsx
+++ packages/react/src/Textarea/Textarea.stories.tsx
@@ -1,102 +1,93 @@
-import { boolean } from '@storybook/addon-knobs';
 import Textarea from '.';
 
 export default {
   title: 'Basic/Textarea',
 };
 
-export const Basic = () => {
-  const error = boolean('error', true);
+export const Basic = () => (
+  <div
+    style={{
+      display: 'inline-grid',
+      gridTemplateColumns: 'repeat(3, 200px)',
+      gridTemplateRows: '60px',
+      gap: '16px',
+      alignItems: 'center',
+    }}
+  >
+    <Textarea
+      placeholder="輸入文字..."
+      maxLength={50}
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      maxLength={50}
+      disabled
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      maxLength={50}
+      error
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      maxLength={50}
+      value="Example"
+      readOnly
+    />
+  </div>
+);
 
-  return (
-    <div
-      style={{
-        display: 'inline-grid',
-        gridTemplateColumns: 'repeat(3, 200px)',
-        gridTemplateRows: '60px',
-        gap: '16px',
-        alignItems: 'center',
-      }}
-    >
-      <Textarea
-        placeholder="輸入文字..."
-        maxLength={50}
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        maxLength={50}
-        disabled
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        maxLength={50}
-        error={error}
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        maxLength={50}
-        value="Example"
-        readOnly
-      />
-    </div>
-  );
-};
-
-export const Sizes = () => {
-  const error = boolean('error', true);
-
-  return (
-    <div
-      style={{
-        display: 'inline-grid',
-        gridTemplateColumns: 'repeat(3, 200px)',
-        gridTemplateRows: '60px',
-        gap: '16px',
-        alignItems: 'center',
-      }}
-    >
-      <Textarea
-        placeholder="輸入文字..."
-        size="small"
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="medium"
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="large"
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="small"
-        disabled
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="medium"
-        disabled
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="large"
-        disabled
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="small"
-        error={error}
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="medium"
-        error={error}
-      />
-      <Textarea
-        placeholder="輸入文字..."
-        size="large"
-        error={error}
-      />
-    </div>
-  );
-};
+export const Sizes = () => (
+  <div
+    style={{
+      display: 'inline-grid',
+      gridTemplateColumns: 'repeat(3, 200px)',
+      gridTemplateRows: '60px',
+      gap: '16px',
+      alignItems: 'center',
+    }}
+  >
+    <Textarea
+      placeholder="輸入文字..."
+      size="small"
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="medium"
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="large"
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="small"
+      disabled
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="medium"
+      disabled
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="large"
+      disabled
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="small"
+      error
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="medium"
+      error
+    />
+    <Textarea
+      placeholder="輸入文字..."
+      size="large"
+      error
+    />
+  </div>
+);
