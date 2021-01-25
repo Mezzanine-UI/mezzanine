9b1e2f35e15b835153da7a098d686de94dffa840
Jay Chen <gcobs135102@gmail.com>
2021-01-25 13:41:22 +0800

    feat(react/tag): implement

---
+++ packages/react/src/Tag/Tag.tsx
@@ -0,0 +1,79 @@
+import {
+  DetailedHTMLProps,
+  forwardRef,
+  HTMLAttributes,
+  MouseEventHandler,
+} from 'react';
+import { tagClasses as classes, TagSize } from '@mezzanine-ui/core/tag';
+import { TimesIcon } from '@mezzanine-ui/icons';
+import { cx } from '../utils/cx';
+import Icon from '../Icon';
+
+export interface TagProps extends DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
+  /**
+   * Whether the tag can be closed.
+   * @default false
+   */
+  closable?: boolean;
+  /**
+   * Whether the tag disabled.
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * Callback executed while tag closed.
+   */
+  onClose?: MouseEventHandler;
+  /**
+   * The size of the tag.
+   * @default 'medium'
+   */
+  size?: TagSize;
+}
+
+/**
+ * The react component for `mezzanine` tag.
+ */
+const Tag = forwardRef<HTMLSpanElement, TagProps>(function Tag(props, ref) {
+  const {
+    children,
+    className,
+    closable = false,
+    disabled = false,
+    onClose,
+    size = 'medium',
+    ...rest
+  } = props;
+
+  return (
+    <span
+      {...rest}
+      ref={ref}
+      aria-disabled={disabled}
+      className={cx(
+        classes.host,
+        classes.size(size),
+        {
+          [classes.disabled]: disabled,
+        },
+        className,
+      )}
+    >
+      <span className={classes.label}>{children}</span>
+      {closable && (
+        <Icon
+          className={classes.closeIcon}
+          icon={TimesIcon}
+          onClick={(event) => {
+            if (!disabled && onClose) {
+              onClose(event);
+            }
+          }}
+          tabIndex={-1}
+        />
+      )}
+    </span>
+  );
+});
+
+export default Tag;
