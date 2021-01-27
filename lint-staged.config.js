66678b6e4a4d1e376ea8043c9659ff02006b423b
Jay Chen <gcobs135102@gmail.com>
2021-01-27 16:42:55 +0800

    chore: change lint-staged for .html

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  '*.@(js|jsx|ts|tsx|html)': ['eslint --ext .js,.jsx,.ts,.tsx --fix'],
-  '*.mdx': ['prettier --write', 'eslint --ext .mdx --fix'],
+  '*.@(js|jsx|ts|tsx)': ['eslint --ext .js,.jsx,.ts,.tsx --fix'],
   '*.@(css|sass|scss)': ['stylelint --fix'],
+  '*.@(html|mdx)': ['prettier --write', 'eslint --ext .html,.mdx --fix'],
   '*.@(json|md)': ['prettier --write'],
 };
