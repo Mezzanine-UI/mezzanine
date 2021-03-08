3985fca53c873f46fa279824bbbdd6c1243ff445
Jay Chen <gcobs135102@gmail.com>
2021-03-08 14:24:16 +0800

    fix(core/empty): add missing color and typography

--- packages/core/src/empty/empty.ts
+++ packages/core/src/empty/empty.ts
@@ -4,5 +4,6 @@ export const emptyClasses = {
   host: emptyPrefix,
   icon: `${emptyPrefix}__icon`,
   title: `${emptyPrefix}__title`,
+  description: `${emptyPrefix}__description`,
   fullHeight: `${emptyPrefix}--full-height`,
 } as const;
