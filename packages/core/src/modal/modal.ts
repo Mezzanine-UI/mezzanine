523d48600daf02c77b1d1b362f26ff043760ba30
Jay Chen <gcobs135102@gmail.com>
2021-03-15 10:11:28 +0800

    refactor(core/modal): add severity to modal
    BREAKING CHANGE: use severity instead of danger

--- packages/core/src/modal/modal.ts
+++ packages/core/src/modal/modal.ts
@@ -1,21 +1,37 @@
+import {
+  CheckCircleFilledIcon,
+  ExclamationCircleFilledIcon,
+  InfoCircleFilledIcon,
+  MinusCircleFilledIcon,
+} from '@mezzanine-ui/icons';
+import { SeverityWithInfo } from '@mezzanine-ui/system/severity';
 import { Size } from '@mezzanine-ui/system/size';
+
+export type ModalSeverity = SeverityWithInfo;
 
 export type ModalSize = Size | 'extraLarge';
 
 export const modalPrefix = 'mzn-modal';
 
+export const modalSeverityIcons = {
+  success: CheckCircleFilledIcon,
+  warning: ExclamationCircleFilledIcon,
+  error: MinusCircleFilledIcon,
+  info: InfoCircleFilledIcon,
+} as const;
+
 export const modalClasses = {
   host: modalPrefix,
   overlay: `${modalPrefix}__overlay`,
   closeIcon: `${modalPrefix}__close-icon`,
+  severity: (severity: ModalSeverity) => `${modalPrefix}--${severity}`,
   size: (size: ModalSize) => `${modalPrefix}--${size === 'extraLarge' ? 'extra-large' : size}`,
-  danger: `${modalPrefix}--danger`,
   fullScreen: `${modalPrefix}--full-screen`,
   withCloseIcon: `${modalPrefix}--close-icon`,
 
   /** Header */
   header: `${modalPrefix}__header`,
-  titleIcon: `${modalPrefix}__title-icon`,
+  severityIcon: `${modalPrefix}__severity-icon`,
   title: `${modalPrefix}__title`,
   titleLarge: `${modalPrefix}__title--large`,
 
@@ -26,3 +42,4 @@ export const modalClasses = {
   footer: `${modalPrefix}__footer`,
   actions: `${modalPrefix}__actions`,
 } as const;
+
