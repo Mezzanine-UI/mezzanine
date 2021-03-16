e95c8474c15fadb6f461fa0550d538376bed725e
Steven Ho <shockshocksoul@gmail.com>
2021-03-16 17:22:52 +0800

    feat(icons): add chevron right and chevron left icon

---
+++ packages/icons/src/chevron-left.ts
@@ -0,0 +1,15 @@
+import { IconDefinition } from './typings';
+
+export const ChevronLeftIcon: IconDefinition = {
+  name: 'chevron-left',
+  definition: {
+    svg: {
+      viewBox: '0 0 24 24',
+    },
+    path: {
+      fill: 'currentColor',
+      fillRule: 'evenodd',
+      d: 'M8 12l7.02-7 .98.976v.002L9.959 12 16 18.026l-.979.974h-.002z',
+    },
+  },
+};
