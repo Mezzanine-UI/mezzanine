2272a629fe805127d3123ac7a498275862677ea0
travor20814 <travor20814@gmail.com>
2021-03-24 16:19:25 +0800

    feat(core/overlay): add invisible backdrop styling class

--- packages/core/src/overlay/overlay.ts
+++ packages/core/src/overlay/overlay.ts
@@ -3,4 +3,5 @@ export const overlayPrefix = 'mzn-overlay';
 export const overlayClasses = {
   host: overlayPrefix,
   backdrop: `${overlayPrefix}__backdrop`,
+  invisible: `${overlayPrefix}__backdrop--invisible`,
 } as const;
