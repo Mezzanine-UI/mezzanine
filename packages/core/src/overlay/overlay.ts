926d4fb6dc7a54cf756c7ab000b8b24bcfe4b1d8
travor20814 <travor20814@gmail.com>
2021-03-25 14:04:55 +0800

    fix(core/overlay): add fixed position styling to fix scrollable container bug
    ref #33
    
    re #33

--- packages/core/src/overlay/overlay.ts
+++ packages/core/src/overlay/overlay.ts
@@ -2,6 +2,8 @@ export const overlayPrefix = 'mzn-overlay';
 
 export const overlayClasses = {
   host: overlayPrefix,
+  hostFixed: `${overlayPrefix}--fixed`,
   backdrop: `${overlayPrefix}__backdrop`,
+  backdropFixed: `${overlayPrefix}__backdrop--fixed`,
   invisible: `${overlayPrefix}__backdrop--invisible`,
 } as const;
