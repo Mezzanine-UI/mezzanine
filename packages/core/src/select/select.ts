b15b66d95b650d5827b811d336a982bd8abd0796
travor20814 <travor20814@gmail.com>
2021-03-31 18:49:07 +0800

    fix(core/select): remove overlay styles in select and set menu width to auto (control by popper)

--- packages/core/src/select/select.ts
+++ packages/core/src/select/select.ts
@@ -6,7 +6,6 @@ export const selectPrefix = 'mzn-select';
 
 export const selectClasses = {
   host: selectPrefix,
-  overlay: `${selectPrefix}__overlay`,
   popper: `${selectPrefix}__popper`,
   suffixIconActive: `${selectPrefix}__text-field__suffix-icon--active`,
   tags: `${selectPrefix}__text-field__tags`,
