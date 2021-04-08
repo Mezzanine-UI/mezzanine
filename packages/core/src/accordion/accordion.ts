6dab6c4d727bd55174c2ce305d2e7c25183471b3
travor20814 <travor20814@gmail.com>
2021-04-08 20:33:48 +0800

    feat(core/accordion): first implementation

---
+++ packages/core/src/accordion/accordion.ts
@@ -0,0 +1,12 @@
+export const accordionPrefix = 'mzn-accordion';
+
+export const accordionClasses = {
+  host: accordionPrefix,
+  summary: `${accordionPrefix}__summary`,
+  summaryDisabled: `${accordionPrefix}__summary--disabled`,
+  summaryIcon: `${accordionPrefix}__summary__icon`,
+  summaryIconDisabled: `${accordionPrefix}__summary__icon--disabled`,
+  summaryIconExpanded: `${accordionPrefix}__summary__icon--expanded`,
+  details: `${accordionPrefix}__details`,
+  detailsExpanded: `${accordionPrefix}__details--expanded`,
+};
