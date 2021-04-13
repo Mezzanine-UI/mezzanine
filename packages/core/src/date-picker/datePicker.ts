4c1a10909a95014864b013a7ecf7a29066ba6038
Kuan <kuan.thisis@gmail.com>
2021-04-13 13:38:43 +0800

    feat(core/date-picker): imlement via design draft

---
+++ packages/core/src/date-picker/datePicker.ts
@@ -0,0 +1,8 @@
+/** Classes */
+export const datePickerPrefix = 'mzn-date-picker';
+export const datePickerCalendarPrefix = `${datePickerPrefix}-calendar`;
+
+export const datePickerClasses = {
+  host: datePickerPrefix,
+  calendar: datePickerCalendarPrefix,
+};
