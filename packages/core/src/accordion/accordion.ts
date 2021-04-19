a567f0c084660abf1882eddb3642a3b17826001a
travor20814 <travor20814@gmail.com>
2021-04-19 15:17:27 +0800

    refactor(core/accordion): refactor by adding mainPart wrapper and prefix slot

--- packages/core/src/accordion/accordion.ts
+++ packages/core/src/accordion/accordion.ts
@@ -7,6 +7,8 @@ export const accordionClasses = {
   summaryIcon: `${accordionPrefix}__summary__icon`,
   summaryIconDisabled: `${accordionPrefix}__summary__icon--disabled`,
   summaryIconExpanded: `${accordionPrefix}__summary__icon--expanded`,
+  summaryMainPart: `${accordionPrefix}__summary__mainPart`,
+  summaryMainPartPrefix: `${accordionPrefix}__summary__mainPart__prefix`,
   details: `${accordionPrefix}__details`,
   detailsExpanded: `${accordionPrefix}__details--expanded`,
 };
