c38b23c3da7db1763b797428143382b8a12a145e
Kuan <kuan.thisis@gmail.com>
2021-05-07 19:56:20 +0800

    feat(core/calendar): add methods of startOf, getter and setters of hour, minute, second

--- packages/core/src/calendar/presets/typings.ts
+++ packages/core/src/calendar/presets/typings.ts
@@ -2,6 +2,9 @@
 export type CalendarMethods<DateType> = {
   /** Get date infos */
   getNow: () => DateType;
+  getSecond: (value: DateType) => number;
+  getMinute: (value: DateType) => number;
+  getHour: (value: DateType) => number;
   getDate: (value: DateType) => number;
   getWeekDay: (value: DateType) => number;
   getMonth: (value: DateType) => number;
@@ -14,9 +17,13 @@ export type CalendarMethods<DateType> = {
   addDay: (value: DateType, diff: number) => DateType;
   addYear: (value: DateType, diff: number) => DateType;
   addMonth: (value: DateType, diff: number) => DateType;
+  setSecond: (value: DateType, second: number) => DateType;
+  setMinute: (value: DateType, minute: number) => DateType;
+  setHour: (value: DateType, hour: number) => DateType;
   setYear: (value: DateType, year: number) => DateType;
   setMonth: (value: DateType, month: number) => DateType;
   setDate: (value: DateType, date: number) => DateType;
+  startOf: (value: DateType, granularity: any) => DateType;
 
   /** Generate day calendar */
   getCalendarGrid: (target: DateType) => number[][];
