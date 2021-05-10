36f5d9dda6726d17cc0ca419bf55eaede3f040bc
Kuan <kuan.thisis@gmail.com>
2021-05-10 14:17:23 +0800

    feat(core/date-time-picker): implement via design draft

---
+++ packages/core/src/date-time-picker/dateTimePicker.ts
@@ -0,0 +1,10 @@
+/** Classes */
+export const dateTimePickerPrefix = 'mzn-date-time-picker';
+export const dateTimePickerPanelPrefix = `${dateTimePickerPrefix}-panel`;
+
+export const dateTimePickerClasses = {
+  /** Panel */
+  panel: dateTimePickerPanelPrefix,
+  panelSelectors: `${dateTimePickerPanelPrefix}__selectors`,
+  panelCalendar: `${dateTimePickerPanelPrefix}__calendar`,
+};
