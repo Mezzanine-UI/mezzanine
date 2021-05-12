f8f58e311da7a0c1370fe23ba38276cff24b80f4
Kuan <kuan.thisis@gmail.com>
2021-05-12 13:46:38 +0800

    feat(core/notification): implement via design draft

---
+++ packages/core/src/notification/notification.ts
@@ -0,0 +1,38 @@
+import {
+  CheckCircleFilledIcon,
+  ExclamationCircleFilledIcon,
+  MinusCircleFilledIcon,
+  InfoCircleFilledIcon,
+} from '@mezzanine-ui/icons';
+import { SeverityWithInfo } from '@mezzanine-ui/system/severity';
+
+export type NotificationSeverity = SeverityWithInfo;
+
+export const notificationPrefix = 'mzn-notif';
+export const notificationRootPrefix = `${notificationPrefix}-root`;
+
+export const notificationIcons = {
+  success: CheckCircleFilledIcon,
+  warning: ExclamationCircleFilledIcon,
+  error: MinusCircleFilledIcon,
+  info: InfoCircleFilledIcon,
+} as const;
+
+export const notificationClasses = {
+  host: notificationPrefix,
+  severity: (severity: NotificationSeverity) => `${notificationPrefix}--${severity}`,
+  closeIcon: `${notificationPrefix}__close-icon`,
+
+  /** Severity Icon classes */
+  iconContainer: `${notificationPrefix}__icon-container`,
+  severityIcon: `${notificationPrefix}__severity-icon`,
+
+  /** Body classes */
+  body: `${notificationPrefix}__body`,
+  title: `${notificationPrefix}__title`,
+  content: `${notificationPrefix}__content`,
+  action: `${notificationPrefix}__action`,
+
+  /** Root classes */
+  root: notificationRootPrefix,
+} as const;
