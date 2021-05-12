728ae68f7277ff6d2981a07fd24d612495559147
BenChen <BenChen@rytass.com>
2021-05-12 17:04:40 +0800

    refactor(react/card): fix element name and struct

--- packages/react/src/Card/Card.tsx
+++ packages/react/src/Card/Card.tsx
@@ -1,37 +1,105 @@
-import { forwardRef, MouseEventHandler } from 'react';
+import { forwardRef, ReactNode, CSSProperties } from 'react';
 import { cardClasses as classes } from '@mezzanine-ui/core/card';
 import { cx } from '../utils/cx';
 import { NativeElementPropsWithoutKeyAndRef } from '../utils/jsx-types';
+import Typography, { TypographyProps } from '../Typography';
 
-export interface CardProps extends NativeElementPropsWithoutKeyAndRef<'div'> {
+export interface CardProps extends Omit<NativeElementPropsWithoutKeyAndRef<'div'>, 'title' | 'children'> {
   /**
-   * Click handler.
+   * Inline style to apply to the card.
    */
-  media?: React.ReactNode;
-  style?: React.CSSProperties;
-  title: string;
-  subhead: string;
-  text: string;
-  footer?: React.ReactNode;
-  onClick?: MouseEventHandler;
+  style?: CSSProperties;
+  /**
+   * Card cover.
+  */
+  cover?: ReactNode;
+  /**
+   * Card title, hiding if the text is empty.
+   */
+  title?: ReactNode;
+  /**
+   * Card title props
+   * @default 'variant: h3'
+   */
+  titleProps?: Omit<TypographyProps, 'children'>;
+  /**
+   * Subtitle under the title
+   */
+  subtitle?: ReactNode;
+  /**
+   * Subtitle props
+   */
+  subtitleProps?: Omit<TypographyProps, 'children'>;
+  /**
+   * Description under the subtitle
+   */
+  description?: string;
+  /**
+   * Description props
+   */
+  descriptionProps?: Omit<TypographyProps, 'children'>;
+  /**
+   * Card footer
+   */
+  actions?: ReactNode;
 }
 
 /**
  * The react component for `mezzanine` card.
  */
-
 const Card = forwardRef<HTMLDivElement, CardProps>(function Card(props, ref) {
   const {
     className,
-    children,
-    media,
-    title = 'Card title',
-    subhead = 'subhead',
-    text = 'text',
-    footer,
-    onClick,
+    cover,
+    title,
+    titleProps,
+    subtitle,
+    subtitleProps,
+    description,
+    descriptionProps,
+    actions,
     ...rest
   } = props;
+
+  const {
+    variant = 'h3',
+  } = titleProps || {};
+
+  const titleTypography = (
+    title ? (
+      <Typography
+        variant={variant}
+        {...titleProps}
+      >
+        {title}
+      </Typography>
+    ) : null
+  );
+
+  const subtitleTypography = (
+    subtitle ? (
+      <Typography {...subtitleProps}>
+        {subtitle}
+      </Typography>
+    ) : null
+  );
+
+  const header = (
+    titleTypography || subtitleTypography ? (
+      <div className={classes.header}>
+        {titleTypography}
+        {subtitleTypography}
+      </div>
+    ) : null
+  );
+
+  const descriptionTypography = (
+    description ? (
+      <Typography {...descriptionProps}>
+        {description}
+      </Typography>
+    ) : null
+  );
 
   return (
     <div
@@ -42,38 +110,15 @@ const Card = forwardRef<HTMLDivElement, CardProps>(function Card(props, ref) {
       )}
       {...rest}
     >
-      <div className={classes.media}>
-        {media}
-      </div>
-
-      <div>{children}</div>
-
-      <div className={classes.cardSpace}>
-        <div className={classes.header}>
-          <div
-            className={classes.title}
-          >
-            {title}
-          </div>
-          <div
-            className={classes.subhead}
-          >
-            {subhead}
-          </div>
+      {cover}
+      <div className={classes.meta}>
+        <div className={classes.content}>
+          {header}
+          {descriptionTypography}
         </div>
-
-        <div
-          className={classes.content}
-        >
-          {text}
-        </div>
-
-        <div className={classes.footer}>
-          {footer}
-        </div>
+        {actions}
       </div>
     </div>
-
   );
 });
 
