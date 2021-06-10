dec39d35f8dd5d0ac18f96ee3b7e21abbe733f20
Kuan <kuan.thisis@gmail.com>
2021-06-10 18:03:11 +0800

    feat(react/form): add focus event handler type

--- packages/react/src/Form/index.ts
+++ packages/react/src/Form/index.ts
@@ -1,3 +1,4 @@
+export * from './typings';
 export * from './FormControlContext';
 export { FormFieldProps, default as FormField } from './FormField';
 export { FormLabelProps, default as FormLabel } from './FormLabel';
