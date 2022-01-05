2f9fa26627a024519d3b2bdc3f6bd6c213d6baa2
TingTing <ting@rytass.com>
2022-01-05 10:51:14 +0800

    feat(core/upload): core of UploadPicture and UploadPictureWall

---
+++ packages/core/src/upload/UploadPictureWall.ts
@@ -0,0 +1,6 @@
+export const uploadPictureWallPrefix = 'mzn-upload-picture-wall';
+
+export const uploadPictureWallClasses = {
+  host: uploadPictureWallPrefix,
+  item: `${uploadPictureWallPrefix}__item`,
+};
