2f0aa49072cc53387ee4079330076d0ddec7a9c3
timchen <timchen10001@gmail.com>
2022-01-27 02:33:15 +0800

    feat(react/input): add tags-mode story of input component

--- packages/react/src/Input/Input.stories.tsx
+++ packages/react/src/Input/Input.stories.tsx
@@ -1,6 +1,8 @@
 import { PlusIcon, SearchIcon } from '@mezzanine-ui/icons';
 import Icon from '../Icon';
 import Input from '.';
+import { Message } from '..';
+import Typography from '../Typography/Typography';
 
 export default {
   title: 'Data Entry/Input',
@@ -139,4 +141,125 @@ export const PrefixSuffix = () => (
       error
     />
   </div>
+);
+
+export const TagsMode = () => (
+  <>
+    <ul>
+      <li>Default Max Tags Length is 3</li>
+      <li>Default Input Max Length is 8</li>
+      <li>
+        Default Position of tags-mode-input field is
+        <b> bottom</b>
+      </li>
+      <li>Tags will be stored in a set (unique value)</li>
+    </ul>
+    <div style={{ height: '20px' }} />
+    <div
+      style={{
+        display: 'inline-grid',
+        gridTemplateColumns: 'repeat(2, 320px)',
+        gap: '16px',
+        alignItems: 'center',
+      }}
+    >
+      <section>
+        <Typography>inputMaxLength=1</Typography>
+        <Input
+          fullWidth
+          mode="tags"
+          placeholder="inputMaxLength=1"
+          inputProps={{ maxLength: 1 }}
+          tagsProps={{
+            onTagsChange: (newValue) => Message.info(JSON.stringify(newValue)),
+          }}
+        />
+      </section>
+      <section>
+        <Typography>maxTagsLength=4, inputMaxLength=3</Typography>
+        <Input
+          mode="tags"
+          placeholder="maxTagsLength=4; inputMaxLength=3"
+          inputProps={{ maxLength: 3 }}
+          tagsProps={{
+            maxTagsLength: 4,
+            onTagsChange: (newValue) => Message.info(JSON.stringify(newValue)),
+          }}
+        />
+      </section>
+      <section>
+        <Typography>with clearable</Typography>
+        <Input
+          clearable
+          error
+          mode="tags"
+          placeholder="with clearable"
+          tagsProps={{
+            onTagsChange: (newValue) => Message.info(JSON.stringify(newValue)),
+          }}
+        />
+      </section>
+      <section>
+        <Typography>with initialTagsValue</Typography>
+        <Input
+          clearable
+          mode="tags"
+          placeholder="with initialTagsValue"
+          tagsProps={{
+            initialTagsValue: ['1', '2', '3', '4'],
+            onTagsChange: (newValue) => Message.info(JSON.stringify(newValue)),
+          }}
+        />
+      </section>
+      <section>
+        <Typography>
+          initialTagsValue will be sliced, once maxTagsLength is given
+          (maxTagsLength: 2)
+        </Typography>
+        <Input
+          clearable
+          mode="tags"
+          placeholder="initialTagsValue will be sliced"
+          tagsProps={{
+            maxTagsLength: 2,
+            initialTagsValue: ['1', '2', '3'],
+            onTagsChange: (newValue) => Message.info(JSON.stringify(newValue)),
+          }}
+        />
+      </section>
+      <section>
+        <Typography>
+          Input Field on Top Position
+        </Typography>
+        <Input
+          clearable
+          mode="tags"
+          placeholder="Input Field on Top Position"
+          tagsProps={{
+            inputPosition: 'top',
+            maxTagsLength: 5,
+            initialTagsValue: ['1', '2', '3', '4'],
+            onTagsChange: (newValue) => Message.info(JSON.stringify(newValue)),
+          }}
+        />
+      </section>
+      <section>
+        <Typography>
+          With Prefix (Origin Input Prop Combination)
+        </Typography>
+        <Input
+          clearable
+          mode="tags"
+          placeholder="Input Field on Top Position"
+          prefix={<Icon icon={SearchIcon} />}
+          tagsProps={{
+            inputPosition: 'top',
+            maxTagsLength: 5,
+            initialTagsValue: ['With', 'Prefix', '!', '!'],
+            onTagsChange: (newValue) => Message.info(JSON.stringify(newValue)),
+          }}
+        />
+      </section>
+    </div>
+  </>
 );
