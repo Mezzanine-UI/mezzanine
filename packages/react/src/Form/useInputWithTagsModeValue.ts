8886275de8e1ea0641cc8052c98657083c8a78b5
timchen <timchen10001@gmail.com>
2022-01-27 02:26:19 +0800

    feat(react/hook): add use-input-with-tags-mode-value hook logic

---
+++ packages/react/src/Form/useInputWithTagsModeValue.ts
@@ -0,0 +1,142 @@
+import {
+  ChangeEvent,
+  KeyboardEvent,
+  RefObject,
+  useCallback,
+  useRef,
+  useState,
+} from 'react';
+import { useInputControlValue, UseInputControlValueProps } from './useInputControlValue';
+
+export interface UseInputWithMultipleSplitValueProps<E extends HTMLInputElement | HTMLTextAreaElement>
+  extends UseInputControlValueProps<E> {
+  ref: RefObject<E>;
+  /**
+   * The value of initial tags
+   */
+  initialTagsValue?: string[];
+  /**
+   * Maximum permissible amount of the tag
+   * @default 3
+   */
+  maxTagsLength?: number;
+  /**
+   * Maximum length of value on each tag
+   * @default 8
+   */
+  tagValueMaxLength?: number;
+  /**
+   * Whether skip the life-cycle methods of this hook
+   * @default false
+   */
+  skip?: boolean;
+  /**
+   * The change event handler of tags
+   */
+  onTagsChange?: (tags: string[]) => void,
+}
+
+export function useInputWithTagsModeValue<E extends HTMLInputElement | HTMLTextAreaElement>(
+  props: Omit<UseInputWithMultipleSplitValueProps<E>, 'onChange'>,
+) {
+  const {
+    defaultValue,
+    onTagsChange: onChangeProp,
+    ref,
+    initialTagsValue = [],
+    skip = false,
+    maxTagsLength,
+    tagValueMaxLength = 8,
+  } = props;
+  const canActive = !skip;
+  const activeMaxTagsLength = maxTagsLength || Math.max(3, initialTagsValue.length);
+  const tagsElementRef = useRef<HTMLDivElement | null>(null);
+  const tagsSetRef = useRef<Set<string>>(new Set(initialTagsValue));
+
+  const generateUniqueTags = () => Array.from(tagsSetRef.current.values());
+
+  const [value, setValue] = useInputControlValue({
+    defaultValue: canActive ? defaultValue : undefined,
+  });
+  const [tags, setTags] = useState<string[]>(
+    generateUniqueTags()
+      .map((initialTag) => initialTag.slice(0, tagValueMaxLength))
+      .slice(0, activeMaxTagsLength),
+  );
+
+  const tagsWillOverflow = useCallback(() => (
+    tags.length === activeMaxTagsLength
+  ), [tags]);
+  const clearTypingFieldValue = () => {
+    if (!canActive) return;
+
+    const target = ref.current;
+
+    if (target) {
+      const changeEvent: ChangeEvent<E> = Object.create({});
+
+      changeEvent.target = target;
+      changeEvent.currentTarget = target;
+
+      target.value = '';
+      setValue(changeEvent);
+    }
+  };
+
+  const onClear = () => {
+    clearTypingFieldValue();
+    tagsSetRef.current.clear();
+    setTags([]);
+    onChangeProp?.([]);
+  };
+
+  const onChange = (event: ChangeEvent<E> | null) => {
+    if (canActive && event) {
+      setValue(event);
+    }
+  };
+
+  const onRemove = (tag: string) => {
+    tagsSetRef.current.delete(tag);
+
+    const newTags = generateUniqueTags();
+
+    setTags(newTags);
+    onChangeProp?.(newTags);
+  };
+
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    if (!canActive) return;
+    const element = ref.current;
+
+    if (
+      element &&
+      element?.value &&
+      e.key === 'Enter' &&
+      !tagsWillOverflow()
+    ) {
+      const tagsSet = tagsSetRef.current;
+
+      tagsSet.add(element.value);
+
+      const newTags = generateUniqueTags();
+
+      setTags(newTags);
+      onChangeProp?.(newTags);
+      clearTypingFieldValue();
+    }
+  }, [tagsWillOverflow]);
+
+  return [
+    {
+      tags,
+      tagsRef: tagsElementRef,
+      typingValue: value,
+      tagsReachedMax: tagsWillOverflow(),
+    },
+    onChange,
+    onClear,
+    onRemove,
+    onKeyDown,
+  ] as const;
+}
