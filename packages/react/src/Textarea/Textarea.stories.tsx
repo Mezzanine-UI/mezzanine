cedfe35fb9eb3cdebccedaa859cc77bad7c710e4
Rosie <syminrosie@gmail.com>
2022-02-11 12:50:38 +0800

    feat(react/text-area): add playground of text-area

--- packages/react/src/Textarea/Textarea.stories.tsx
+++ packages/react/src/Textarea/Textarea.stories.tsx
@@ -1,7 +1,38 @@
-import Textarea from '.';
+import { Story } from '@storybook/react';
+import Textarea, { TextareaProps, TextareaSize } from '.';
 
 export default {
   title: 'Data Entry/Textarea',
+};
+
+const sizes: TextareaSize[] = [
+  'small',
+  'medium',
+  'large',
+];
+
+export const Playground: Story<TextareaProps> = ({ ...props }) => (
+  <Textarea {...props} />
+);
+
+Playground.args = {
+  clearable: false,
+  disabled: false,
+  readOnly: false,
+  error: false,
+  fullWidth: false,
+  placeholder: '輸入文字...',
+  maxLength: 50,
+  size: 'medium',
+};
+
+Playground.argTypes = {
+  size: {
+    control: {
+      type: 'select',
+      options: sizes,
+    },
+  },
 };
 
 export const Basic = () => (
