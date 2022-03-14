e4df1e0b8cfe54d534f219f7caccff3d3cfdaaa9
TingTing <ting@rytass.com>
2022-03-14 16:08:15 +0800

    chore: testing of SelectTriggerTags

--- packages/react/src/Select/SelectTriggerTags.spec.tsx
+++ packages/react/src/Select/SelectTriggerTags.spec.tsx
@@ -19,6 +19,47 @@ describe('<SelectTriggerTags />', () => {
     ),
   );
 
+  it('has no tags if value not given', () => {
+    const { getHostHTMLElement } = render(
+      <SelectTriggerTags
+        ellipsis={false}
+      />,
+    );
+
+    const tags = getHostHTMLElement().getElementsByClassName('mzn-tag');
+
+    expect(tags.length).toBe(0);
+  });
+
+  describe('click close icon of tags without onTagClose', () => {
+    ([true, false]).forEach((isEllipsis) => {
+      it(`case: ellipsis="${isEllipsis}"`, async () => {
+        const { getHostHTMLElement } = render(
+          <SelectTriggerTags
+            ellipsis={isEllipsis}
+            value={[{
+              id: 'foo',
+              name: 'foo',
+            }]}
+          />,
+        );
+
+        const tags = getHostHTMLElement().getElementsByClassName('mzn-tag');
+
+        expect(tags.length).toBe(1);
+
+        const firstTag = tags[0];
+        const firstTagCloseIcon = firstTag.getElementsByClassName('mzn-tag__close-icon')[0];
+
+        await act(async () => {
+          fireEvent.click(firstTagCloseIcon!);
+        });
+
+        expect(tags.length).toBe(1);
+      });
+    });
+  });
+
   it('invoke onTagClose when click close icon of tags', async () => {
     const onTagClose = jest.fn();
 
