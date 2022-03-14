fcfcfe95193419786dd49c880f8749b815850069
TingTing <ting@rytass.com>
2022-03-14 15:18:16 +0800

    chore: testing of SelectTriggerTags

--- packages/react/src/Select/SelectTriggerTags.spec.tsx
+++ packages/react/src/Select/SelectTriggerTags.spec.tsx
@@ -1,6 +1,8 @@
 import {
+  act,
   cleanupHook,
   render,
+  fireEvent,
 } from '../../__test-utils__';
 import {
   describeForwardRefToHTMLElement,
@@ -16,4 +18,32 @@ describe('<SelectTriggerTags />', () => {
       <SelectTriggerTags ref={ref} ellipsis />,
     ),
   );
+
+  it('invoke onTagClose when click close icon of tags', async () => {
+    const onTagClose = jest.fn();
+
+    const { getHostHTMLElement } = render(
+      <SelectTriggerTags
+        ellipsis
+        onTagClose={onTagClose}
+        value={[{
+          id: 'foo',
+          name: 'foo',
+        }]}
+      />,
+    );
+
+    const tags = getHostHTMLElement().getElementsByClassName('mzn-tag');
+
+    expect(tags.length).toBe(1);
+
+    const firstTag = tags[0];
+    const firstTagCloseIcon = firstTag.getElementsByClassName('mzn-tag__close-icon')[0];
+
+    await act(async () => {
+      fireEvent.click(firstTagCloseIcon!);
+    });
+
+    expect(onTagClose).toBeCalledTimes(1);
+  });
 });
