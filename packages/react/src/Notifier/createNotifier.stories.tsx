2523b1b1c2842dd757fc062c5cdd36d9c8f68a81
TingTing <ting@rytass.com>
2022-05-27 15:13:09 +0800

    refactor(react/notifier): use new createRoot api / refactor testing code

--- packages/react/src/Notifier/createNotifier.stories.tsx
+++ packages/react/src/Notifier/createNotifier.stories.tsx
@@ -19,7 +19,9 @@ const Notifier = createNotifier<TestNotifierData>({
 export const Common = () => {
   const [messageKeys, setMessageKeys] = useState<Key[]>([]);
 
-  useEffect(() => () => Notifier.destroy(), []);
+  useEffect(() => () => {
+    Notifier.destroy();
+  }, []);
 
   return (
     <ButtonGroup style={{ marginBottom: 16 }}>
