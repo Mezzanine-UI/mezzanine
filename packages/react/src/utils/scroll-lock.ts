6cbda2fe2a57b26bd93572a735c425d884a4a445
travor20814 <travor20814@gmail.com>
2024-08-16 10:13:12 +0800

    fix(react/utils): get scrollbar width should called before body locked

--- packages/react/src/utils/scroll-lock.ts
+++ packages/react/src/utils/scroll-lock.ts
@@ -3,12 +3,11 @@ import { getScrollbarWidth } from './get-scrollbar-width';
 export function lockBodyScroll() {
   const { scrollY } = window;
 
-  document.body.style.position = 'fixed';
-  document.body.style.top = `-${scrollY}px`;
-
   // Calculate scroll bar width and use padding-right to remain layout width.
   const scrollbarWidth = getScrollbarWidth();
 
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollY}px`;
   document.body.style.paddingRight = `${scrollbarWidth}px`;
   document.body.style.overflow = 'hidden';
 }
