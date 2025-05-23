833946aa031102a56ccc13517028103354edc317
travor20814 <travor20814@gmail.com>
2025-05-23 13:35:08 +0800

    build: remove deprecated @testing-library/react-hook library

--- packages/react/__test-utils__/core.ts
+++ packages/react/__test-utils__/core.ts
@@ -12,10 +12,7 @@ export {
   default as TestRenderer,
 } from 'react-test-renderer';
 export * from '@testing-library/react';
-export {
-  renderHook,
-  cleanup as cleanupHook,
-} from '@testing-library/react-hooks';
+export { renderHook, cleanup as cleanupHook } from '@testing-library/react';
 
 export type RenderResult<Q extends Queries = typeof queries> =
   CoreRenderResult<Q> & {
