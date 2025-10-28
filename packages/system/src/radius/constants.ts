4df6c2151b02d8ca0b6aa6f1539382c3da4116a4
travor20814 <travor20814@gmail.com>
2025-10-28 14:39:47 +0800

    feat(system/radius): implement radius config

---
+++ packages/system/src/radius/constants.ts
@@ -0,0 +1,1 @@
+export const radiusPrefix = 'mzn-radius';
---
+++ packages/system/src/radius/index.ts
@@ -0,0 +1,9 @@
+export type { RadiusSize, RadiusConfig } from './radius';
+export { radiusPrefix } from './constants';
+export {
+  defaultRadius,
+  radiusSizes,
+  isValidRadiusSize,
+  getRadiusVarName,
+  getRadiusVar,
+} from './radius';
