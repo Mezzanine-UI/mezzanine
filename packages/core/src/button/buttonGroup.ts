bdb86fb14503ec456c7d68ba963588e78e442a06
travor20814 <travor20814@gmail.com>
2025-10-30 16:46:36 +0800

    feat(core/button): refactor button to apply new design system

--- packages/core/src/button/buttonGroup.ts
+++ packages/core/src/button/buttonGroup.ts
@@ -1,35 +1,13 @@
-import { CssVarInterpolations } from '@mezzanine-ui/system/css';
 import { Orientation } from '@mezzanine-ui/system/orientation';
-import { SpacingLevel, toSpacingCssVar } from '@mezzanine-ui/system/spacing';
-import { buttonPrefix, ButtonSize } from './button';
+import { buttonPrefix } from './button';
 
 export type ButtonGroupOrientation = Orientation;
-export type ButtonGroupSpacing = SpacingLevel;
-
-export interface ButtonGroupCssVars {
-  size: ButtonSize;
-  spacing?: ButtonGroupSpacing;
-}
 
 export const buttonGroupPrefix = `${buttonPrefix}-group` as const;
 
 export const buttonGroupClasses = {
   host: buttonGroupPrefix,
   fullWidth: `${buttonGroupPrefix}--full-width`,
-  orientation: (orientation: ButtonGroupOrientation) => `${buttonGroupPrefix}--${orientation}`,
-  attached: `${buttonGroupPrefix}--attached`,
+  orientation: (orientation: ButtonGroupOrientation) =>
+    `${buttonGroupPrefix}--${orientation}`,
 } as const;
-
-export function toButtonGroupCssVars(variables: ButtonGroupCssVars): CssVarInterpolations {
-  const {
-    size,
-    spacing,
-  } = variables;
-
-  return {
-    /**
-     * The default is `3` for button size="small", or `4`.
-     */
-    [`--${buttonGroupPrefix}-spacing`]: toSpacingCssVar(spacing ?? (size === 'small' ? 3 : 4)),
-  };
-}
