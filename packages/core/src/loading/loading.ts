3b39acc68d90008d28c8023dee76fbc2759b9760
travor20814 <travor20814@gmail.com>
2025-11-05 15:58:59 +0800

    feat(core/loading): refactor loading to fit new design system

--- packages/core/src/loading/loading.ts
+++ packages/core/src/loading/loading.ts
@@ -1,9 +1,12 @@
+import { GeneralSize } from '@mezzanine-ui/system/size';
+
 export const loadingPrefix = 'mzn-loading';
 
 export const iconClasses = {
   host: loadingPrefix,
   stretch: `${loadingPrefix}--stretch`,
   spin: `${loadingPrefix}__spin`,
+  size: (size: GeneralSize) => `${loadingPrefix}__spin--${size}`,
   icon: `${loadingPrefix}__spin__icon`,
-  tip: `${loadingPrefix}__spin__tip`,
+  description: `${loadingPrefix}__spin__description`,
 } as const;
