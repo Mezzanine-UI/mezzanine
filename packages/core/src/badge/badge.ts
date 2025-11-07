8104b3dd722d08a104ae815a8f2fc36903080dd2
Kris Tseng <kris@rytass.com>
2025-11-07 12:00:07 +0800

    feat(core/badge): update badge with new design system

--- packages/core/src/badge/badge.ts
+++ packages/core/src/badge/badge.ts
@@ -1,8 +1,23 @@
 export const badgePrefix = 'mzn-badge';
 
+export type BadgeVariant =
+  // dot types
+  | 'dot-success'
+  | 'dot-error'
+  | 'dot-warning'
+  | 'dot-info'
+  | 'dot-inactive'
+
+  // count types
+  | 'count-alert'
+  | 'count-inactive'
+  | 'count-inverse'
+  | 'count-brand'
+  | 'count-info';
+
 export const badgeClasses = {
   host: badgePrefix,
+  variant: (variant: BadgeVariant) => `${badgePrefix}--${variant}`,
   container: `${badgePrefix}__container`,
-  dot: `${badgePrefix}--dot`,
   hide: `${badgePrefix}--hide`,
-} as const;
+};
