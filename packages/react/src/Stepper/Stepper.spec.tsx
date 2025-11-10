5caa2107e312298f14118fcffefb674805cb0661
BenChen <ans10528@gmail.com>
2025-11-10 13:29:48 +0800

    test(react/stepper): add stepper test and stories

--- packages/react/src/Stepper/Stepper.spec.tsx
+++ packages/react/src/Stepper/Stepper.spec.tsx
@@ -1,5 +1,3 @@
-import { PlusIcon } from '@mezzanine-ui/icons';
-import { IconProps } from '../Icon';
 import { cleanup, render } from '../../__test-utils__';
 import {
   describeForwardRefToHTMLElement,
@@ -12,7 +10,7 @@ describe('<Stepper />', () => {
 
   describeForwardRefToHTMLElement(HTMLDivElement, (ref) =>
     render(
-      <Stepper ref={ref}>
+      <Stepper ref={ref} type={'number'} orientation={'horizontal'}>
         <Step />
       </Stepper>,
     ),
@@ -20,46 +18,29 @@ describe('<Stepper />', () => {
 
   describeHostElementClassNameAppendable('foo', (className) =>
     render(
-      <Stepper className={className}>
+      <Stepper className={className} type={'number'} orientation={'horizontal'}>
         <Step />
       </Stepper>,
     ),
   );
 
-  const testStepper = () => {
-    const completedIconProps: IconProps = { icon: PlusIcon };
+  ['number', 'dot'].forEach((type) => {
+    ['horizontal', 'vertical'].forEach((orientation) => {
+      it(`should render correct UI when type is ${type} and orientation is ${orientation}`, () => {
+        const { container } = render(
+          <Stepper
+            processingStep={2}
+            type={type as 'number' | 'dot'}
+            orientation={orientation as 'horizontal' | 'vertical'}
+          >
+            <Step key="step1" title="step1" />
+            <Step key="step2" title="step2" />
+            <Step key="step3" title="step3" />
+          </Stepper>,
+        );
 
-    return (
-      <Stepper activeStep={1}>
-        <Step key="step1" title="step1" />
-        <Step key="step2" title="step2" />
-        <Step key="step3" title="step3" />
-        <Step
-          key="step4"
-          title="step4"
-          completedIconProps={completedIconProps}
-          completed
-        />
-      </Stepper>
-    );
-  };
-
-  describe('children step', () => {
-    it('should render step number and set state', () => {
-      const { getHostHTMLElement } = render(testStepper());
-      const element = getHostHTMLElement();
-
-      expect(element.childNodes[0].childNodes[0].textContent).toBe('');
-      expect(element.childNodes[1].childNodes[0].textContent).toBe('2');
-      expect(element.childNodes[2].childNodes[0].textContent).toBe('3');
-    });
-    it('should render the step icon', () => {
-      const { getHostHTMLElement } = render(testStepper());
-      const element = getHostHTMLElement();
-
-      expect(
-        element.querySelector('.mzn-stepper-step__completed-icon'),
-      ).toBeTruthy();
+        expect(container.firstChild).toBeTruthy();
+      });
     });
   });
 });
--- packages/react/src/Stepper/Stepper.stories.tsx
+++ packages/react/src/Stepper/Stepper.stories.tsx
@@ -1,7 +1,5 @@
 import { Meta, StoryFn } from '@storybook/react-webpack5';
 import { useState } from 'react';
-import { PlusIcon } from '@mezzanine-ui/icons';
-import { IconProps } from '../Icon';
 import Stepper from './Stepper';
 import Step from './Step';
 import Typography from '../Typography';
@@ -12,52 +10,69 @@ export default {
 } as Meta;
 
 const exampleSteps = ['步驟一', '步驟二', '步驟三'];
-
-export const Basic = () => {
-  const completedIconProps: IconProps = { icon: PlusIcon };
-
-  return (
-    <Stepper activeStep={1}>
-      <Step key="step1" title="step1" />
-      <Step key="step2" title="step2" />
-      <Step key="step3" title="step3" />
-      <Step
-        key="step4"
-        title="step4"
-        completedIconProps={completedIconProps}
-        completed
-      />
-    </Stepper>
-  );
-};
-
-export const State = () => (
-  <div
-    style={{
-      boxSizing: 'border-box',
-      width: '100%',
-    }}
-  >
-    {Array.from(Array(5)).map((_, idx) => (
-      <Stepper key={`${idx.toString()}`} activeStep={idx - 1}>
-        {exampleSteps.map((label) => (
-          <Step key={label} title={label} />
-        ))}
-      </Stepper>
-    ))}
-  </div>
-);
+const exampleStepsDescription = ['步驟一敘述', '步驟二敘述', '步驟三敘述'];
 
 interface PlaygroundArgs {
   stepCount: number;
-  titleVariant: any;
 }
 
-export const Playground: StoryFn<PlaygroundArgs> = ({
-  stepCount,
-  titleVariant,
-}) => {
-  const [current, setCurrent] = useState(-1);
+export const Status = () => (
+  <div
+    style={{
+      display: 'grid',
+      gap: '32px',
+    }}
+  >
+    <Typography variant="h3">{`Status`}</Typography>
+
+    <div
+      style={{
+        display: 'grid',
+        gap: '24px',
+      }}
+    >
+      <Stepper orientation="horizontal" type="number" processingStep={1}>
+        <Step title={'processing'} status="processing" />
+        <Step title={'pending'} status="pending" />
+        <Step title={'succeeded'} status="succeeded" />
+        <Step title={'error'} status="error" />
+        <Step title={'disabled'} status="disabled" />
+      </Stepper>
+      <Stepper orientation="horizontal" type="dot" processingStep={1}>
+        <Step title={'processing'} status="processing" />
+        <Step title={'pending'} status="pending" />
+        <Step title={'succeeded'} status="succeeded" />
+        <Step title={'error'} status="error" />
+        <Step title={'disabled'} status="disabled" />
+      </Stepper>
+    </div>
+
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+      }}
+    >
+      <Stepper orientation="vertical" type="number" processingStep={1}>
+        <Step title={'processing'} status="processing" />
+        <Step title={'pending'} status="pending" />
+        <Step title={'succeeded'} status="succeeded" />
+        <Step title={'error'} status="error" />
+        <Step title={'disabled'} status="disabled" />
+      </Stepper>
+      <Stepper orientation="vertical" type="dot" processingStep={1}>
+        <Step title={'processing'} status="processing" />
+        <Step title={'pending'} status="pending" />
+        <Step title={'succeeded'} status="succeeded" />
+        <Step title={'error'} status="error" />
+        <Step title={'disabled'} status="disabled" />
+      </Stepper>
+    </div>
+  </div>
+);
+
+export const Playground: StoryFn<PlaygroundArgs> = ({ stepCount }) => {
+  const [current, setCurrent] = useState(1);
 
   const next = () => {
     setCurrent(current + 1);
@@ -70,38 +85,85 @@ export const Playground: StoryFn<PlaygroundArgs> = ({
   const storyStepCount = Math.max(stepCount, 0);
 
   return (
-    <div>
-      <Stepper activeStep={current}>
-        {Array.from(Array(storyStepCount)).map((_, idx) => (
-          <Step
-            key={exampleSteps[idx % 3] + idx.toString()}
-            title={exampleSteps[idx % 3]}
-            titleProps={{ variant: titleVariant }}
-          />
-        ))}
-      </Stepper>
-      <Typography align="center" variant="h4">
-        activeStepIndex:
-        {current}
-      </Typography>
-      <ButtonGroup color="primary" variant="contained" size="medium">
-        <Button onClick={prev}>Prev</Button>
-        <Button onClick={next}>Next</Button>
-      </ButtonGroup>
+    <div style={{ display: 'grid', gap: '32px' }}>
+      {/* step controller */}
+      <div style={{ display: 'grid', gap: '24px' }}>
+        <Typography variant="h3">{`processingStep: ${current}`}</Typography>
+        <ButtonGroup color="primary">
+          <Button onClick={prev}>Prev</Button>
+          <Button onClick={next}>Next</Button>
+        </ButtonGroup>
+      </div>
+
+      <br />
+
+      <div
+        style={{
+          display: 'grid',
+          gap: '24px',
+        }}
+      >
+        <Stepper
+          orientation="horizontal"
+          type="number"
+          processingStep={current}
+        >
+          {Array.from(Array(storyStepCount)).map((_, idx) => (
+            <Step
+              key={exampleSteps[idx % 3] + idx.toString()}
+              title={exampleSteps[idx % 3]}
+              description={exampleStepsDescription[idx % 3]}
+              onClick={() => alert(`Clicked step ${idx + 1}`)}
+            />
+          ))}
+        </Stepper>
+        <Stepper orientation="horizontal" type="dot" processingStep={current}>
+          {Array.from(Array(storyStepCount)).map((_, idx) => (
+            <Step
+              key={exampleSteps[idx % 3] + idx.toString()}
+              title={exampleSteps[idx % 3]}
+              description={exampleStepsDescription[idx % 3]}
+              onClick={() => alert(`Clicked step ${idx + 1}`)}
+            />
+          ))}
+        </Stepper>
+      </div>
+
+      <br />
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Stepper orientation="vertical" type="number" processingStep={current}>
+          {Array.from(Array(storyStepCount)).map((_, idx) => (
+            <Step
+              key={exampleSteps[idx % 3] + idx.toString()}
+              title={exampleSteps[idx % 3]}
+              description={exampleStepsDescription[idx % 3]}
+              onClick={() => alert(`Clicked step ${idx + 1}`)}
+            />
+          ))}
+        </Stepper>
+        <Stepper orientation="vertical" type="dot" processingStep={current}>
+          {Array.from(Array(storyStepCount)).map((_, idx) => (
+            <Step
+              key={exampleSteps[idx % 3] + idx.toString()}
+              title={exampleSteps[idx % 3]}
+              description={exampleStepsDescription[idx % 3]}
+              onClick={() => alert(`Clicked step ${idx + 1}`)}
+            />
+          ))}
+        </Stepper>
+      </div>
     </div>
   );
 };
 
 Playground.args = {
-  titleVariant: 'button2',
   stepCount: 4,
 };
 
-Playground.argTypes = {
-  titleVariant: {
-    control: {
-      type: 'inline-radio',
-      options: ['button2', 'button3', 'button4'],
-    },
-  },
-};
+Playground.argTypes = {};
