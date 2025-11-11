dd9500302aa610ef1e5feac89deda4c746729f30
akai.chang <akai.chang@rytass.com>
2025-11-11 17:50:14 +0800

    fix(core/dismissButton): export type definitions for shared usage

--- packages/core/src/dismiss-button/dismissButton.ts
+++ packages/core/src/dismiss-button/dismissButton.ts
@@ -1,10 +1,19 @@
+export type DismissButtonType = 'embedded' | 'standard';
+
+export type DismissButtonEmbeddedVariant = 'contrast' | 'emphasis';
+
+export type DismissButtonStandardVariant = 'base' | 'inverse';
+
+export type DismissButtonVariant =
+  | DismissButtonEmbeddedVariant
+  | DismissButtonStandardVariant;
+
 export const dismissButtonPrefix = 'mzn-dismiss-button';
 
 export const dismissButtonClasses = {
   host: dismissButtonPrefix,
   icon: `${dismissButtonPrefix}__icon`,
-  type: (type: 'embedded' | 'standard') =>
-    `${dismissButtonPrefix}--type-${type}`,
-  variant: (variant: 'base' | 'contrast' | 'emphasis' | 'inverse') =>
+  type: (type: DismissButtonType) => `${dismissButtonPrefix}--type-${type}`,
+  variant: (variant: DismissButtonVariant) =>
     `${dismissButtonPrefix}--variant-${variant}`,
 } as const;
