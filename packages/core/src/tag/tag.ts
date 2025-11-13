dbb57dca26ea08b37a001f490fbb9458526b6b40
Kris Tseng <kris@rytass.com>
2025-11-13 18:13:54 +0800

    feat(core/tag): update tag style for new design system

--- packages/core/src/tag/tag.ts
+++ packages/core/src/tag/tag.ts
@@ -1,13 +1,23 @@
-import { Size } from '@mezzanine-ui/system/size';
-
-export type TagSize = Size;
+import { GeneralSize } from '@mezzanine-ui/system/size';
 
 export const tagPrefix = 'mzn-tag';
 
+export type TagType =
+  | 'static'
+  | 'counter'
+  | 'overflow-counter'
+  | 'dismissable'
+  | 'addable';
+
+export type TagSize = Exclude<GeneralSize, 'minor'>;
+
 export const tagClasses = {
   host: tagPrefix,
-  label: `${tagPrefix}__label`,
-  closeIcon: `${tagPrefix}__close-icon`,
-  disabled: `${tagPrefix}--disabled`,
+  type: (type: TagType) => `${tagPrefix}--${type}`,
   size: (size: TagSize) => `${tagPrefix}--${size}`,
+  label: `${tagPrefix}__label`,
+  icon: `${tagPrefix}__icon`,
+  active: `${tagPrefix}--active`,
+  readOnly: `${tagPrefix}--read-only`,
+  disabled: `${tagPrefix}--disabled`,
 } as const;
