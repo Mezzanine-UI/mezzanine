00a37a6b5ffbd4e4c900e18dd3b435a8a339cba4
Kris Tseng <kris@rytass.com>
2025-11-21 18:56:16 +0800

    feat(react/textarea): textarea v2 component

--- packages/react/src/Textarea/Textarea.tsx
+++ packages/react/src/Textarea/Textarea.tsx
@@ -1,166 +1,79 @@
 'use client';
 
-import { forwardRef, Ref, useContext, ChangeEventHandler, useRef } from 'react';
-import {
-  textareaClasses as classes,
-  TextareaSize,
-} from '@mezzanine-ui/core/textarea';
+import { forwardRef, Ref, useRef } from 'react';
+import { textareaClasses as classes } from '@mezzanine-ui/core/textarea';
 import { cx } from '../utils/cx';
 import { NativeElementPropsWithoutKeyAndRef } from '../utils/jsx-types';
 import { useComposeRefs } from '../hooks/useComposeRefs';
-import { useInputWithClearControlValue } from '../Form/useInputWithClearControlValue';
-import { FormControlContext } from '../Form';
-import TextField, { TextFieldProps } from '../TextField';
-import { MezzanineConfig } from '../Provider/context';
+import TextField from '../TextField';
 
-export interface TextareaProps
-  extends Omit<
-    TextFieldProps,
-    'active' | 'children' | 'onClear' | 'prefix' | 'suffix' | 'suffixActionIcon'
-  > {
-  /**
-   * The default value of textarea.
-   */
-  defaultValue?: string;
-  /**
-   * The max length of textarea.
-   */
-  maxLength?: number;
-  /**
-   * The change event handler of textarea element.
-   */
-  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
-  /**
-   * The placeholder of textarea.
-   */
-  placeholder?: string;
-  /**
-   * Whether the textarea is readonly.
-   * @default false
-   */
-  readOnly?: boolean;
-  /**
-   * Whether the input is required.
-   * @default false
-   */
-  required?: boolean;
-  /**
-   * The rows of textarea.
-   */
-  rows?: number;
-  /**
-   * The size of textarea.
-   * @default 'medium'
-   */
-  size?: TextareaSize;
-  /**
-   * The react ref passed to textarea element.
-   */
+export type TextareaProps = Omit<
+  NativeElementPropsWithoutKeyAndRef<'textarea'>,
+  'disabled' | 'readOnly'
+> & {
+  /** ClassName apply to textarea */
+  textareaClassName?: string;
+  /** The react ref passed to textarea element. */
   textareaRef?: Ref<HTMLTextAreaElement>;
-  /**
-   * The other native props for textarea element.
-   */
-  textareaProps?: Omit<
-    NativeElementPropsWithoutKeyAndRef<'textarea'>,
-    | 'defaultValue'
-    | 'disabled'
-    | 'maxLength'
-    | 'onChange'
-    | 'placeholder'
-    | 'readOnly'
-    | 'required'
-    | 'rows'
-    | 'value'
-    | `aria-${'disabled' | 'multiline' | 'readonly' | 'required'}`
-  >;
-  /**
-   * The value of textarea.
-   */
-  value?: string;
-}
+  /** Controls textarea CSS `resize` behavior with native `resize` values. */
+  resize?: 'none' | 'both' | 'horizontal' | 'vertical';
+} & (
+    | {
+        /**  Set visual style type of the textarea. */
+        type?: 'default';
+        /** Disabled state of the taxtarea, Only available on default type. */
+        disabled?: boolean;
+        /** ReadOnly state of the textarea, Only available on default type. */
+        readOnly?: boolean;
+      }
+    | { type: 'warning' | 'error'; disabled?: never; readOnly?: never }
+  );
 
 /**
  * The react component for `mezzanine` textarea.
  */
 const Textarea = forwardRef<HTMLDivElement, TextareaProps>(
   function Textarea(props, ref) {
-    const { size: globalSize } = useContext(MezzanineConfig);
-    const {
-      disabled: disabledFromFormControl,
-      fullWidth: fullWidthFromFormControl,
-      required: requiredFromFormControl,
-      severity,
-    } = useContext(FormControlContext) || {};
     const {
       className,
-      clearable = false,
-      defaultValue,
-      disabled = disabledFromFormControl || false,
-      error = severity === 'error' || false,
-      fullWidth = fullWidthFromFormControl || false,
-      maxLength,
-      onChange: onChangeProp,
-      placeholder,
-      readOnly = false,
-      required = requiredFromFormControl || false,
-      rows,
-      size = globalSize,
+      disabled,
+      readOnly,
+      textareaClassName,
       textareaRef: textareaRefProp,
-      textareaProps,
-      value: valueProp,
+      type = 'default',
+      resize,
+      ...textareaProps
     } = props;
     const textareaRef = useRef<HTMLTextAreaElement>(null);
-    const [value, onChange, onClear] = useInputWithClearControlValue({
-      defaultValue,
-      onChange: onChangeProp,
-      ref: textareaRef,
-      value: valueProp,
-    });
     const composedTextareaRef = useComposeRefs([textareaRefProp, textareaRef]);
-    const active = !!value;
-    const currentLength = value.length;
-    const upperLimit =
-      typeof maxLength === 'number' && currentLength >= maxLength;
+
+    const interactiveProps =
+      type === 'default'
+        ? disabled
+          ? ({ disabled: true } as const)
+          : readOnly
+            ? ({ readonly: true } as const)
+            : {}
+        : {};
 
     return (
       <TextField
+        {...interactiveProps}
         ref={ref}
-        active={active}
-        className={cx(
-          classes.host,
-          {
-            [classes.upperLimit]: upperLimit,
-          },
-          className,
-        )}
-        clearable={clearable}
-        disabled={disabled}
-        error={error}
-        fullWidth={fullWidth}
-        onClear={onClear}
-        size={size}
+        className={cx(classes.host, className)}
+        error={type === 'error'}
+        warning={type === 'warning'}
       >
         <textarea
           {...textareaProps}
+          className={cx(classes.textarea, textareaClassName)}
           ref={composedTextareaRef}
-          aria-disabled={disabled}
-          aria-multiline
-          aria-readonly={readOnly}
-          aria-required={required}
           disabled={disabled}
-          maxLength={maxLength}
-          onChange={onChange}
-          placeholder={placeholder}
           readOnly={readOnly}
-          required={required}
-          rows={rows}
-          value={value}
+          style={{ resize, ...textareaProps.style }}
         />
-        {typeof maxLength === 'number' && (
-          <span className={classes.count}>
-            {value.length}/{maxLength}
-          </span>
-        )}
+
+        {/* <Icon icon={ResizeHandleIcon} size={16} className={classes.resizer} /> */}
       </TextField>
     );
   },
--- packages/react/src/Textarea/index.ts
+++ packages/react/src/Textarea/index.ts
@@ -1,3 +1,2 @@
-export type { TextareaSize } from '@mezzanine-ui/core/textarea';
 export type { TextareaProps } from './Textarea';
 export { default } from './Textarea';
