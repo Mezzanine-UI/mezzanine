848954bf349b31244dbd9a18200781ebfd5dfc3e
Kris Tseng <kris@rytass.com>
2025-11-22 16:32:31 +0800

    refactor(react/overflow-fooltip): revise storybook args for OverflowTooltip

--- packages/react/src/OverflowTooltip/OverflowTooltip.stories.tsx
+++ packages/react/src/OverflowTooltip/OverflowTooltip.stories.tsx
@@ -13,11 +13,19 @@ type TooltipStory = StoryObj<OverflowTooltipProps>;
 
 export const Playground: TooltipStory = {
   args: {
-    open: false,
-    tags: ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5'],
+    className: '',
     onTagDismiss: (tagIndex: number) => {
       // eslint-disable-next-line no-console
       console.log(`Dismiss tag at index: ${tagIndex}`);
+    },
+    open: true,
+    placement: 'top-start',
+    tags: ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5'],
+    tagSize: 'main',
+  },
+  parameters: {
+    controls: {
+      include: ['className', 'placement', 'tagSize', 'tags', 'open'],
     },
   },
   render: function Render(args) {
@@ -34,7 +42,7 @@ export const Playground: TooltipStory = {
             backgroundColor: 'white',
           }}
         />
-        <OverflowTooltip {...args} anchor={anchorRef} open={true} />
+        <OverflowTooltip {...args} anchor={anchorRef} />
       </div>
     );
   },
@@ -44,18 +52,24 @@ type CounterTagStory = StoryObj<typeof OverflowCounterTag>;
 
 export const OverflowCounterTagPlayground: CounterTagStory = {
   args: {
+    className: '',
+    onTagDismiss: (tagIndex: number) => {
+      // eslint-disable-next-line no-console
+      console.log(`Dismiss tag at index: ${tagIndex}`);
+    },
     tags: ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5'],
     tagSize: 'main',
+    placement: 'top-start',
+  },
+  parameters: {
+    component: OverflowCounterTag,
+    controls: {
+      include: ['className', 'placement', 'tagSize', 'tags'],
+    },
   },
   render: (args) => (
     <div style={{ padding: '100px' }}>
-      <OverflowCounterTag
-        {...args}
-        onTagDismiss={(index) => {
-          // eslint-disable-next-line no-console
-          console.log(`Dismiss tag at index: ${index}`);
-        }}
-      />
+      <OverflowCounterTag {...args} />
     </div>
   ),
 };
