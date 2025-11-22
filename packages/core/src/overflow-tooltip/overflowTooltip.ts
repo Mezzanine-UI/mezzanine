9a8524fbf7d6ae8628928010a2401875b7d01882
Kris Tseng <kris@rytass.com>
2025-11-22 14:00:20 +0800

    feat(core/overflow-tooltip): add styles of OverflowTooltip

---
+++ packages/core/src/overflow-tooltip/overflowTooltip.ts
@@ -0,0 +1,7 @@
+export const overflowTooltipPrefix = 'mzn-overflow-tooltip';
+
+export const overflowTooltipClasses = {
+  host: overflowTooltipPrefix,
+  content: `${overflowTooltipPrefix}__content`,
+  arrow: `${overflowTooltipPrefix}__arrow`,
+};
