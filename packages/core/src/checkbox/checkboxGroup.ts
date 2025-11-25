386e24e3efed85f06c2c005ebce502b417f43b81
akai.chang <akai.chang@rytass.com>
2025-11-25 16:07:55 +0800

    feat(core/checkbox): implement checkbox core styles and types

---
+++ packages/core/src/checkbox/checkboxGroup.ts
@@ -0,0 +1,21 @@
+import { Orientation } from '@mezzanine-ui/system/orientation';
+import { CheckboxMode, checkboxPrefix } from './checkbox';
+
+export type CheckboxGroupLayout = Orientation;
+
+export interface CheckboxGroupOption {
+  disabled?: boolean;
+  mode?: CheckboxMode;
+  label: string | number;
+  value: string;
+}
+
+export const checkboxGroupPrefix = `${checkboxPrefix}-group` as const;
+
+export const checkboxGroupClasses = {
+  host: checkboxGroupPrefix,
+  layout: (layout: CheckboxGroupLayout) => `${checkboxGroupPrefix}--${layout}`,
+  mode: (mode: CheckboxMode) => `${checkboxGroupPrefix}--${mode}`,
+  nested: `${checkboxGroupPrefix}--nested`,
+  contentWrapper: `${checkboxGroupPrefix}--content-wrapper`,
+} as const;
