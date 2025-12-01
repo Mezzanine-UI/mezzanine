da5e8f62c794220408452ab5d55b637311c906ad
travor20814 <travor20814@gmail.com>
2025-12-01 17:31:04 +0800

    feat(core/picker): picker styling

--- packages/core/src/picker/picker.ts
+++ packages/core/src/picker/picker.ts
@@ -6,12 +6,18 @@ export const pickerPrefix = 'mzn-picker';
 export const pickerClasses = {
   host: pickerPrefix,
   arrowIcon: `${pickerPrefix}__arrow-icon`,
+  inputMono: `${pickerPrefix}__input-mono`,
+  formattedInput: `${pickerPrefix}__formatted-input`,
+  formattedInputHidden: `${pickerPrefix}__formatted-input-hidden`,
+  formattedInputDisplay: `${pickerPrefix}__formatted-input-display`,
+  formattedInputSegment: `${pickerPrefix}__formatted-input-segment`,
+  formattedInputSegmentFilled: `${pickerPrefix}__formatted-input-segment--filled`,
 } as const;
 
 /** Types */
 export type RangePickerValue<T = DateType> = undefined[] | [T, T];
 export type RangePickerPickingValue<T = DateType> =
- | RangePickerValue
- | [T]
- | [undefined, T]
- | [T, undefined];
+  | RangePickerValue
+  | [T]
+  | [undefined, T]
+  | [T, undefined];
