a6f37171179169b03864c44a95201b4c102e11fb
Kris Tseng <kris@rytass.com>
2025-12-11 14:53:52 +0800

    fix(core/overflow-tooltip): keep counter tag cursor pointer in readonly state

--- packages/core/src/overflow-tooltip/overflowTooltip.ts
+++ packages/core/src/overflow-tooltip/overflowTooltip.ts
@@ -1,7 +1,11 @@
 export const overflowTooltipPrefix = 'mzn-overflow-tooltip';
+export const overflowCouterTagPrefix = 'mzn-overflow-counter-tag';
 
 export const overflowTooltipClasses = {
   host: overflowTooltipPrefix,
   content: `${overflowTooltipPrefix}__content`,
   arrow: `${overflowTooltipPrefix}__arrow`,
+  counterTagHost: `${overflowCouterTagPrefix}`,
+  counterTagDisabled: `${overflowCouterTagPrefix}--disabled`,
+  counterTagReadOnly: `${overflowCouterTagPrefix}--readonly`,
 };
