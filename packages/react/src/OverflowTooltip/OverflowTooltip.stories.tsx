f7492d7c853d6f80edee7bedd85c03bf1a93e381
Kris Tseng <kris@rytass.com>
2025-12-11 12:14:56 +0800

    feat(react/overflow-tooltip): add readOnly state and simplify width handling

--- packages/react/src/OverflowTooltip/OverflowTooltip.stories.tsx
+++ packages/react/src/OverflowTooltip/OverflowTooltip.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react-webpack5';
 import OverflowTooltip, { OverflowTooltipProps } from '.';
 import { OverflowCounterTag } from '.';
 import { useRef } from 'react';
+import Typography from '../Typography';
 
 export default {
   title: 'Internal/OverflowTooltip',
@@ -43,6 +44,65 @@ export const Playground: TooltipStory = {
           }}
         />
         <OverflowTooltip {...args} anchor={anchorRef} />
+      </div>
+    );
+  },
+};
+
+export const States: TooltipStory = {
+  args: {
+    className: '',
+    open: true,
+    placement: 'top-start',
+    tags: ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5'],
+    tagSize: 'main',
+  },
+  parameters: { controls: { disable: true } },
+
+  render: function Render(args) {
+    const anchor1Ref = useRef<HTMLDivElement | null>(null);
+    const anchor2Ref = useRef<HTMLDivElement | null>(null);
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '100px',
+          paddingTop: '100px',
+        }}
+      >
+        <div
+          ref={anchor1Ref}
+          style={{
+            width: '32px',
+            height: '32px',
+            borderRadius: '999px',
+            backgroundColor: 'white',
+          }}
+        >
+          <Typography variant="h2">Enabled</Typography>
+        </div>
+        <OverflowTooltip
+          {...args}
+          anchor={anchor1Ref}
+          onTagDismiss={(tagIndex: number) => {
+            // eslint-disable-next-line no-console
+            console.log(`Dismiss tag at index: ${tagIndex}`);
+          }}
+        />
+
+        <div
+          ref={anchor2Ref}
+          style={{
+            width: 'fit-content',
+            borderRadius: '999px',
+            backgroundColor: 'white',
+          }}
+        >
+          <Typography variant="h2">Read only</Typography>
+        </div>
+        <OverflowTooltip {...args} anchor={anchor2Ref} readOnly />
       </div>
     );
   },
