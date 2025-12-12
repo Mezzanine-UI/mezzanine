9dd8b48b232bd4a879f1a4ea8477e9f5bfc371d1
BenChen <ans10528@gmail.com>
2025-12-12 22:14:24 +0800

    feat(core/pagination): v2 styles implement

--- packages/core/src/pagination/pagination.ts
+++ packages/core/src/pagination/pagination.ts
@@ -4,6 +4,7 @@ export const paginationClasses = {
   host: paginationPrefix,
   container: `${paginationPrefix}__container`,
   item: `${paginationPrefix}__item`,
+  itemList: `${paginationPrefix}__item-list`,
   jumper: `${paginationPrefix}__jumper`,
   pageSize: `${paginationPrefix}__page-size`,
 } as const;
