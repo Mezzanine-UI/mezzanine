862ebfcaca89bc540c9fdf2fbf68b977f8235753
TingTing <ting@rytass.com>
2025-12-17 16:19:57 +0800

    refactor(core/radio): radio layout

--- packages/core/src/radio/radio.ts
+++ packages/core/src/radio/radio.ts
@@ -1,9 +1,13 @@
-import { InputCheckGroupOrientation, InputCheckSize } from '../_internal/input-check';
+import {
+  InputCheckGroupOrientation,
+  InputCheckSize,
+} from '../_internal/input-check';
 
 export type RadioSize = InputCheckSize;
 
 export interface RadioGroupOption {
   disabled?: boolean;
+  error?: boolean;
   label: string | number;
   value: string;
 }
@@ -15,4 +19,5 @@ export const radioPrefix = 'mzn-radio';
 export const radioClasses = {
   host: radioPrefix,
   checked: `${radioPrefix}--checked`,
+  error: `${radioPrefix}--error`,
 } as const;
