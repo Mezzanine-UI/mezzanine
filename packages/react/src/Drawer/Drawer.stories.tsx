7341d75218c69420f8df22bd7567d864bafe2c8c
MartinChu <hunter77613@hotmail.com>
2025-12-24 12:56:44 +0800

    refactor(react/drawer): updates the Drawer's Storybook story to utilize the `StoryObj` type

--- packages/react/src/Drawer/Drawer.stories.tsx
+++ packages/react/src/Drawer/Drawer.stories.tsx
@@ -1,76 +1,74 @@
-import { useState, useCallback } from 'react';
-import { StoryFn, Meta } from '@storybook/react-webpack5';
+import { useCallback, useState } from 'react';
+import { StoryObj } from '@storybook/react-webpack5';
 import Drawer, { DrawerProps } from '.';
 import { Button } from '../index';
 
 export default {
   title: 'Navigation/Drawer',
-} as Meta;
-
-type PlaygroundStoryArgs = DrawerProps;
-
-export const Playground: StoryFn<PlaygroundStoryArgs> = ({
-  disableCloseOnBackdropClick,
-  disableCloseOnEscapeKeyDown,
-  size,
-}) => {
-  const [open, setOpen] = useState(false);
-
-  const handleClose = useCallback(() => setOpen(false), []);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  return (
-    <>
-      <Button onClick={() => handleClick()} variant="base-text-link">
-        OPEN
-      </Button>
-
-      <Drawer
-        disableCloseOnBackdropClick={disableCloseOnBackdropClick}
-        disableCloseOnEscapeKeyDown={disableCloseOnEscapeKeyDown}
-        onClose={handleClose}
-        open={open}
-        header={{ title: 'Drawer Title' }}
-        size={size}
-        bottom={{
-          ghostActionText: '更多選項',
-          onGhostActionClick: handleClose,
-          secondaryActionText: '取消',
-          onSecondaryActionClick: handleClose,
-          primaryActionText: '儲存變更',
-          onPrimaryActionClick: handleClose,
-        }}
-      >
-        content
-      </Drawer>
-    </>
-  );
 };
 
-Playground.args = {
-  disableCloseOnBackdropClick: false,
-  disableCloseOnEscapeKeyDown: false,
-  size: 'medium',
-};
+export const Playground: StoryObj<DrawerProps> = {
+  argTypes: {
+    disableCloseOnBackdropClick: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    disableCloseOnEscapeKeyDown: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    size: {
+      control: {
+        type: 'radio',
+      },
+      options: ['narrow', 'medium', 'wide'],
+    },
+  },
+  args: {
+    disableCloseOnBackdropClick: false,
+    disableCloseOnEscapeKeyDown: false,
+    size: 'medium',
+  },
+  render: function Render({
+    disableCloseOnBackdropClick,
+    disableCloseOnEscapeKeyDown,
+    size,
+  }) {
+    const [open, setOpen] = useState(false);
 
-Playground.argTypes = {
-  disableCloseOnBackdropClick: {
-    control: {
-      type: 'boolean',
-    },
-  },
-  disableCloseOnEscapeKeyDown: {
-    control: {
-      type: 'boolean',
-    },
-  },
-  size: {
-    control: {
-      type: 'radio',
-    },
-    options: ['narrow', 'medium', 'wide'],
+    const handleClose = useCallback(() => setOpen(false), []);
+
+    const handleClick = () => {
+      setOpen(true);
+    };
+
+    return (
+      <>
+        <Button onClick={() => handleClick()} variant="base-text-link">
+          OPEN
+        </Button>
+
+        <Drawer
+          bottom={{
+            ghostActionText: '更多選項',
+            onGhostActionClick: handleClose,
+            onPrimaryActionClick: handleClose,
+            onSecondaryActionClick: handleClose,
+            primaryActionText: '儲存變更',
+            secondaryActionText: '取消',
+          }}
+          disableCloseOnBackdropClick={disableCloseOnBackdropClick}
+          disableCloseOnEscapeKeyDown={disableCloseOnEscapeKeyDown}
+          header={{ title: 'Drawer Title' }}
+          onClose={handleClose}
+          open={open}
+          size={size}
+        >
+          content
+        </Drawer>
+      </>
+    );
   },
 };
