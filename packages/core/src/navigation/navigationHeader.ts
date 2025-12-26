40d29c3861ddbf10f7207c2a918b73d780be3c4d
BenChen <ans10528@gmail.com>
2025-12-26 19:15:02 +0800

    feat(core/navigation): header styles

--- packages/core/src/navigation/navigationHeader.ts
+++ packages/core/src/navigation/navigationHeader.ts
@@ -4,4 +4,7 @@ export const navigationHeaderPrefix = `${navigationPrefix}-header` as const;
 
 export const navigationHeaderClasses = {
   host: navigationHeaderPrefix,
+  siderIcon: `${navigationHeaderPrefix}__sider-icon`,
+  logo: `${navigationHeaderPrefix}__logo`,
+  content: `${navigationHeaderPrefix}__content`,
 } as const;
