c2083dd5ee68252af976ae5db310cbe75f3f801d
BenChen <ans10528@gmail.com>
2025-12-30 17:01:24 +0800

    feat(react/navigation): nav footer

--- packages/react/src/Navigation/NavigationFooter.tsx
+++ packages/react/src/Navigation/NavigationFooter.tsx
@@ -1,28 +1,38 @@
-import { forwardRef, ReactNode } from 'react';
+import { Children, forwardRef, isValidElement } from 'react';
 import { NativeElementPropsWithoutKeyAndRef } from '../utils/jsx-types';
 import { cx } from '../utils/cx';
 import { navigationFooterClasses as classes } from '@mezzanine-ui/core/navigation';
+import NavigationUserMenu from './NavigationUserMenu';
 
-export interface NavigationFooterProps
-  extends Omit<NativeElementPropsWithoutKeyAndRef<'header'>, 'onClick'> {
-  /**
-   * Whether the item is active.
-   */
-  active?: boolean;
-  /**
-   * Item display content.
-   */
-  children?: ReactNode;
-}
+export type NavigationFooterProps =
+  NativeElementPropsWithoutKeyAndRef<'footer'>;
 
-const NavigationFooter = forwardRef<HTMLHeadElement, NavigationFooterProps>(
+const resolveChildren = (children: React.ReactNode) => {
+  let userMenu: React.ReactNode = null;
+  const otherChildren: React.ReactNode[] = [];
+
+  Children.map(children, (child) => {
+    if (isValidElement(child) && child.type === NavigationUserMenu) {
+      userMenu = child;
+    } else {
+      otherChildren.push(child);
+    }
+  });
+
+  return { userMenu, otherChildren };
+};
+
+const NavigationFooter = forwardRef<HTMLElement, NavigationFooterProps>(
   (props, ref) => {
-    const { active, children, className, ...rest } = props;
+    const { children, className, ...rest } = props;
+
+    const { userMenu, otherChildren } = resolveChildren(children);
 
     return (
-      <header {...rest} ref={ref} className={cx(classes.host, className)}>
-        {children}
-      </header>
+      <footer {...rest} ref={ref} className={cx(classes.host, className)}>
+        {userMenu}
+        <span className={classes.icons}>{otherChildren}</span>
+      </footer>
     );
   },
 );
