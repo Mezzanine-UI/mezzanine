7bd8708013422b445e774032dc905b2f1966a44b
BenChen <ans10528@gmail.com>
2025-12-30 17:33:14 +0800

    feat(core/navigation): nav footer

--- packages/core/src/navigation/navigationFooter.ts
+++ packages/core/src/navigation/navigationFooter.ts
@@ -4,4 +4,5 @@ export const navigationFooterPrefix = `${navigationPrefix}-footer` as const;
 
 export const navigationFooterClasses = {
   host: navigationFooterPrefix,
+  icons: `${navigationFooterPrefix}__icons`,
 } as const;
