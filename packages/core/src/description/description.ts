f0387c701c2abab4f55f61b11f4e881e2992e954
TingTing <ting@rytass.com>
2026-01-05 11:24:41 +0800

    feat(core/description): description title layout

--- packages/core/src/description/description.ts
+++ packages/core/src/description/description.ts
@@ -2,8 +2,11 @@ import { GeneralSize } from '@mezzanine-ui/system/size';
 
 export type DescriptionSize = Extract<GeneralSize, 'main' | 'sub'>;
 
-export const descriptionPrefix = 'mzn-description';
+export const descriptionTitlePrefix = 'mzn-description-title';
+export const descriptionContentPrefix = 'mzn-description-content';
 
 export const descriptionClasses = {
-  host: descriptionPrefix,
+  titleHost: descriptionTitlePrefix,
+  titleText: `${descriptionTitlePrefix}__text`,
+  contentHost: descriptionContentPrefix,
 } as const;
