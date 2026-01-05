d7c2c7a63a29d3007c4ceb834cf87aef749e2732
TingTing <ting@rytass.com>
2026-01-05 15:06:42 +0800

    feat(core/description): define width type

--- packages/core/src/description/description.ts
+++ packages/core/src/description/description.ts
@@ -1,6 +1,7 @@
 import { GeneralSize } from '@mezzanine-ui/system/size';
 
 export type DescriptionSize = Extract<GeneralSize, 'main' | 'sub'>;
+export type DescriptionWidthType = 'narrow' | 'wide' | 'stretch' | 'hug';
 
 export const descriptionTitlePrefix = 'mzn-description-title';
 export const descriptionContentPrefix = 'mzn-description-content';
@@ -8,5 +9,7 @@ export const descriptionContentPrefix = 'mzn-description-content';
 export const descriptionClasses = {
   titleHost: descriptionTitlePrefix,
   titleText: `${descriptionTitlePrefix}__text`,
+  titleWidth: (width: DescriptionWidthType) =>
+    `${descriptionTitlePrefix}--${width}`,
   contentHost: descriptionContentPrefix,
 } as const;
