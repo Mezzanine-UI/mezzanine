199cea8cf56011edd44461f73817879b8b2b7862
TingTing <ting@rytass.com>
2026-01-05 16:25:46 +0800

    feat(core/description): define group host class name

--- packages/core/src/description/description.ts
+++ packages/core/src/description/description.ts
@@ -1,15 +1,22 @@
+import { Orientation } from '@mezzanine-ui/system/orientation';
 import { GeneralSize } from '@mezzanine-ui/system/size';
 
 export type DescriptionSize = Extract<GeneralSize, 'main' | 'sub'>;
 export type DescriptionWidthType = 'narrow' | 'wide' | 'stretch' | 'hug';
+export type DescriptionOrientation = Orientation;
 
 export const descriptionTitlePrefix = 'mzn-description-title';
 export const descriptionContentPrefix = 'mzn-description-content';
+export const descriptionGroupPrefix = 'mzn-description-group';
 
 export const descriptionClasses = {
+  // title
   titleHost: descriptionTitlePrefix,
   titleText: `${descriptionTitlePrefix}__text`,
   titleWidth: (width: DescriptionWidthType) =>
     `${descriptionTitlePrefix}--${width}`,
+  // content
   contentHost: descriptionContentPrefix,
+  // group
+  groupHost: descriptionGroupPrefix,
 } as const;
