903166a465f1a71bcbac4d6d1449b9a40a6ade71
MartinChu <hunter77613@hotmail.com>
2026-01-06 22:01:37 +0800

    fix(react/form): removes severity from FormControl context

--- packages/react/src/Form/FormControlContext.ts
+++ packages/react/src/Form/FormControlContext.ts
@@ -1,13 +1,11 @@
 'use client';
 
-import { SeverityWithInfo } from '@mezzanine-ui/system/severity';
 import { createContext } from 'react';
 
 export interface FormControl {
   disabled: boolean;
   fullWidth: boolean;
   required: boolean;
-  severity?: SeverityWithInfo;
 }
 
 export const FormControlContext = createContext<FormControl | undefined>(
