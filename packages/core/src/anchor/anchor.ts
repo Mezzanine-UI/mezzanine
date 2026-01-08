b20bf14ed07c1ddef6f79be731e937e5486e590d
Rosie <syminrosie@gmail.com>
2026-01-09 03:32:42 +0800

    feat(core/anchor): rename anchor class to anchorItem for consistency and clarity

--- packages/core/src/anchor/anchor.ts
+++ packages/core/src/anchor/anchor.ts
@@ -2,10 +2,9 @@ export const anchorPrefix = 'mzn-anchor';
 
 export const anchorClasses = {
   host: anchorPrefix,
-  anchor: `${anchorPrefix}__anchor`,
-  anchorActive: `${anchorPrefix}__anchor--active`,
-  anchorDisabled: `${anchorPrefix}__anchor--disabled`,
-  defaultPadding: `${anchorPrefix}__anchor--default-padding`,
+  anchorItem: `${anchorPrefix}__anchorItem`,
+  anchorItemActive: `${anchorPrefix}__anchorItem--active`,
+  anchorItemDisabled: `${anchorPrefix}__anchorItem--disabled`,
   nested: `${anchorPrefix}__nested`,
   nestedLevel1: `${anchorPrefix}__nested--level-1`,
   nestedLevel2: `${anchorPrefix}__nested--level-2`,
