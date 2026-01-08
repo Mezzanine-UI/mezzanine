178d03d9d3359529dceba25096e139ffe66fc80b
Rosie <syminrosie@gmail.com>
2026-01-09 01:28:46 +0800

    feat(core/anchor): enhance styles and update class structure for improved layout

--- packages/core/src/anchor/anchor.ts
+++ packages/core/src/anchor/anchor.ts
@@ -2,7 +2,11 @@ export const anchorPrefix = 'mzn-anchor';
 
 export const anchorClasses = {
   host: anchorPrefix,
-  bar: `${anchorPrefix}__bar`,
   anchor: `${anchorPrefix}__anchor`,
   anchorActive: `${anchorPrefix}__anchor--active`,
+  anchorDisabled: `${anchorPrefix}__anchor--disabled`,
+  defaultPadding: `${anchorPrefix}__anchor--default-padding`,
+  nested: `${anchorPrefix}__nested`,
+  nestedLevel1: `${anchorPrefix}__nested--level-1`,
+  nestedLevel2: `${anchorPrefix}__nested--level-2`,
 } as const;
