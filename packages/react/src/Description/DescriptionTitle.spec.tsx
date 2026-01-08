a37bd2498d8cc371947afea646695a5f012822a0
TingTing <ting@rytass.com>
2026-01-08 16:35:35 +0800

    feat(react/description): test title

--- packages/react/src/Description/DescriptionTitle.spec.tsx
+++ packages/react/src/Description/DescriptionTitle.spec.tsx
@@ -1,4 +1,5 @@
 import { cleanup, render } from '../../__test-utils__';
+import { QuestionOutlineIcon } from '@mezzanine-ui/icons';
 import {
   describeForwardRefToHTMLElement,
   describeHostElementClassNameAppendable,
@@ -15,4 +16,34 @@ describe('<DescriptionTitle />', () => {
   describeHostElementClassNameAppendable('foo', (className) =>
     render(<DescriptionTitle className={className}>title</DescriptionTitle>),
   );
+
+  it('should bind host class', () => {
+    const { getHostHTMLElement } = render(
+      <DescriptionTitle>title</DescriptionTitle>,
+    );
+    const element = getHostHTMLElement();
+
+    expect(element!.classList.contains('mzn-description-title')).toBeTruthy();
+  });
+
+  it('badge should be set when badge prop is given', () => {
+    const { getHostHTMLElement } = render(
+      <DescriptionTitle badge="dot-success">title</DescriptionTitle>,
+    );
+    const element = getHostHTMLElement();
+
+    expect(
+      element.firstElementChild!.classList.contains('mzn-badge__container'),
+    ).toBeTruthy();
+  });
+
+  it('icon should be set when icon prop is given', () => {
+    const { getHostHTMLElement } = render(
+      <DescriptionTitle icon={QuestionOutlineIcon}>title</DescriptionTitle>,
+    );
+    const element = getHostHTMLElement();
+    const [iconElement] = element.getElementsByTagName('i');
+
+    expect(iconElement!.classList.contains('mzn-icon')).toBeTruthy();
+  });
 });
