d52f2cb82e82d3e56444eba5e4c8abef3062f3f4
BenChen <ans10528@gmail.com>
2026-01-21 01:41:19 +0800

    refactor(core/navigation): consolidate navigation style files

--- packages/core/src/navigation/index.ts
+++ packages/core/src/navigation/index.ts
@@ -1,7 +1,1 @@
 export * from './navigation';
-export * from './navigationFooter';
-export * from './navigationHeader';
-export * from './navigationIconButton';
-export * from './navigationOption';
-export * from './navigationUserMenu';
-export * from './navigationOptionCategory';
--- packages/core/src/navigation/navigationFooter.ts
+++
@@ -1,9 +0,0 @@
-import { navigationPrefix } from './navigation';
-
-export const navigationFooterPrefix = `${navigationPrefix}-footer` as const;
-
-export const navigationFooterClasses = {
-  host: navigationFooterPrefix,
-  collapsed: `${navigationFooterPrefix}--collapsed`,
-  icons: `${navigationFooterPrefix}__icons`,
-} as const;
