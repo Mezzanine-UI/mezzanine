c6bb80df393a7bdc01e6a01d4916f255a680f304
BenChen <ans10528@gmail.com>
2026-01-23 09:18:24 +0800

    feat(core/content-header): content-header implement

--- packages/core/src/page-toolbar/pageToolbar.ts
+++
@@ -1,8 +0,0 @@
-export const pageToolbarPrefix = 'mzn-page-toolbar';
-
-type PageToolbarSize = 'main' | 'sub';
-
-export const pageToolbarClasses = {
-  host: pageToolbarPrefix,
-  size: (size: PageToolbarSize) => `${pageToolbarPrefix}--${size}`,
-} as const;
