0542b03d1ce51b9a009d2c88b3f955110fd56713
BenChen <ans10528@gmail.com>
2026-01-26 06:01:27 +0800

    feat(core/breadcrumb): add overflow menu with dropdown

--- packages/core/src/breadcrumb/breadcrumb.ts
+++ packages/core/src/breadcrumb/breadcrumb.ts
@@ -2,6 +2,10 @@ export const breadcrumbPrefix = 'mzn-breadcrumb';
 
 export const breadcrumbClasses = {
   host: breadcrumbPrefix,
+  iconButton: `${breadcrumbPrefix}__icon-button`,
+  menu: `${breadcrumbPrefix}__menu`,
+  menuContent: `${breadcrumbPrefix}__menu__content`,
+  menuItem: `${breadcrumbPrefix}__menu-item`,
 } as const;
 
 export const breadcrumbItemClasses = {
@@ -10,7 +14,11 @@ export const breadcrumbItemClasses = {
   current: `${breadcrumbPrefix}__item--current`,
   expanded: `${breadcrumbPrefix}__item--expanded`,
   icon: `${breadcrumbPrefix}__item__icon`,
-  menu: `${breadcrumbPrefix}__item__menu`,
-  menuItem: `${breadcrumbPrefix}__item__menu-item`,
-  menuItemTrigger: `${breadcrumbPrefix}__item__menu-item__trigger`,
+} as const;
+
+export const breadcrumbOverflowMenuItemClasses = {
+  host: `${breadcrumbPrefix}__overflow-menu-item`,
+  trigger: `${breadcrumbPrefix}__overflow-menu-item__trigger`,
+  expanded: `${breadcrumbPrefix}__overflow-menu-item--expanded`,
+  icon: `${breadcrumbPrefix}__overflow-menu-item__icon`,
 } as const;
