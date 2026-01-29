5707a9a5e5adfbf42a5314adf1c7384c03e3f16f
travor20814 <travor20814@gmail.com>
2026-01-29 13:26:43 +0800

    feat(core/date-time-range-picker): implement date time range picker styles

---
+++ packages/core/src/date-time-range-picker/dateTimeRangePicker.ts
@@ -0,0 +1,8 @@
+export const dateTimeRangePickerPrefix = 'mzn-date-time-range-picker';
+
+export const dateTimeRangePickerClasses = {
+  host: dateTimeRangePickerPrefix,
+  row: `${dateTimeRangePickerPrefix}--row`,
+  column: `${dateTimeRangePickerPrefix}--column`,
+  arrow: `${dateTimeRangePickerPrefix}__arrow`,
+} as const;
