2c42329567c6f8f46c32f4237e969052ce9621a2
akai.chang <akai.chang@rytass.com>
2026-01-30 10:28:29 +0800

    fix(core/autocomplete): add min-width styles for single and multiple modes
    - Add min-width styles for single and multiple modes
    - Add hostMode class helper function

--- packages/core/src/autocomplete/autocomplete.ts
+++ packages/core/src/autocomplete/autocomplete.ts
@@ -12,4 +12,5 @@ export const autocompleteClasses = {
   host: autocompletePrefix,
   hostFullWidth: `${autocompletePrefix}--full-width`,
   hostInsideClosed: `${autocompletePrefix}--inside-closed`,
+  hostMode: (mode: AutoCompleteMode) => `${autocompletePrefix}--${mode}`,
 } as const;
