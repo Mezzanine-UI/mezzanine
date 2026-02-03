4e4912d18a795a26ac6631d2fe054e2528562f7c
travor20814 <travor20814@gmail.com>
2026-02-03 15:35:06 +0800

    feat(core/floating-button): floating button styles

---
+++ packages/core/src/floating-button/floatingButton.ts
@@ -0,0 +1,7 @@
+export const floatingButtonPrefix = 'mzn-floating-button';
+
+export const floatingButtonClasses = {
+  host: floatingButtonPrefix,
+  button: `${floatingButtonPrefix}__button`,
+  buttonHidden: `${floatingButtonPrefix}__button--hidden`,
+};
