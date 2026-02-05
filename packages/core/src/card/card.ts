98e5567f06d479fd5fd97ecd177787768f8131d5
travor20814 <travor20814@gmail.com>
2026-02-05 13:26:23 +0800

    refactor(core/card): remove deprecated card styles

--- packages/core/src/card/card.ts
+++
@@ -1,8 +0,0 @@
-export const cardPrefix = 'mzn-card';
-
-export const cardClasses = {
-  host: cardPrefix,
-  meta: `${cardPrefix}__meta`,
-  metaContents: `${cardPrefix}__metaContents`,
-  metaContentsHeader: `${cardPrefix}__metaContentsHeader`,
-} as const;
--- packages/core/src/card/cardActions.ts
+++
@@ -1,7 +0,0 @@
-
-import { cardPrefix } from './card';
-
-export const cardActionsPrefix = `${cardPrefix}-actions` as const;
-export const cardActionsClasses = {
-  host: cardActionsPrefix,
-} as const;
