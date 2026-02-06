44c7b2bb35db6679c0cb051227481e5ded4585c7
BenChen <ans10528@gmail.com>
2026-02-06 15:42:43 +0800

    feat(core/content-header): responsive breakpoint

--- packages/core/src/content-header/contentHeader.ts
+++ packages/core/src/content-header/contentHeader.ts
@@ -1,10 +1,18 @@
 export const contentHeaderPrefix = 'mzn-content-header';
 
+export type ResponsiveBreakpoint =
+  | 'above1080px'
+  | 'above680px'
+  | 'below1080px'
+  | 'below680px'
+  | 'between680and1080px';
+
 type ContentHeaderSize = 'main' | 'sub';
 
 export const contentHeaderClasses = {
   host: contentHeaderPrefix,
-  breakpoint: (breakpoint: string) => `${contentHeaderPrefix}--${breakpoint}`,
+  breakpoint: (breakpoint: ResponsiveBreakpoint) =>
+    `${contentHeaderPrefix}--${breakpoint}`,
   size: (size: ContentHeaderSize) => `${contentHeaderPrefix}--${size}`,
   titleArea: `${contentHeaderPrefix}__title-area`,
   textGroup: `${contentHeaderPrefix}__text-group`,
