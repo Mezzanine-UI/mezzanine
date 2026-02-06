af90a092a33dce2174dfdd46f7c8be2c8a0f740d
travor20814 <travor20814@gmail.com>
2026-02-06 15:57:18 +0800

    feat(core/card): implement quick action card styles

--- packages/core/src/card/card.ts
+++ packages/core/src/card/card.ts
@@ -1,18 +1,30 @@
 export const cardGroupPrefix = 'mzn-card-group';
 export const baseCardPrefix = 'mzn-base-card';
+export const quickActionCardPrefix = 'mzn-quick-action-card';
 
 export const cardClasses = {
   /** group */
   group: cardGroupPrefix,
+  groupQuickAction: `${cardGroupPrefix}--quick-action`,
 
   /** base card */
   base: baseCardPrefix,
-  disabled: `${baseCardPrefix}--disabled`,
-  readOnly: `${baseCardPrefix}--read-only`,
+  baseDisabled: `${baseCardPrefix}--disabled`,
+  baseReadOnly: `${baseCardPrefix}--read-only`,
   baseHeader: `${baseCardPrefix}__header`,
   baseHeaderContentWrapper: `${baseCardPrefix}__header__content-wrapper`,
   baseHeaderTitle: `${baseCardPrefix}__header__content-wrapper__title`,
   baseHeaderDescription: `${baseCardPrefix}__header__content-wrapper__description`,
   baseHeaderAction: `${baseCardPrefix}__header__action`,
   baseContent: `${baseCardPrefix}__content`,
+
+  /** quick action card */
+  quickAction: quickActionCardPrefix,
+  quickActionDisabled: `${quickActionCardPrefix}--disabled`,
+  quickActionReadOnly: `${quickActionCardPrefix}--read-only`,
+  quickActionIcon: `${quickActionCardPrefix}__icon`,
+  quickActionContent: `${quickActionCardPrefix}__content`,
+  quickActionTitle: `${quickActionCardPrefix}__content__title`,
+  quickActionSubtitle: `${quickActionCardPrefix}__content__subtitle`,
+  quickActionVertical: `${quickActionCardPrefix}--vertical`,
 };
