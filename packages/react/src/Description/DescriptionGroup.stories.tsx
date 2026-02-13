7eaaefb2bef93df213fc5ba1ad31447d20ad563c
TingTing <ting@rytass.com>
2026-02-13 13:46:55 +0800

    fix(react/description): fix DescriptionGroup storybook

--- packages/react/src/Description/DescriptionGroup.stories.tsx
+++ packages/react/src/Description/DescriptionGroup.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react-webpack5';
+import Badge from '../Badge';
 import Description from './Description';
+import DescriptionContent from './DescriptionContent';
 import DescriptionGroup from './DescriptionGroup';
 
 export default {
@@ -11,37 +13,15 @@ type GroupStory = StoryObj<typeof DescriptionGroup>;
 export const Playground: GroupStory = {
   render: () => (
     <DescriptionGroup>
-      <Description
-        titleProps={{
-          widthType: 'narrow',
-          children: '訂購日期',
-        }}
-        contentProps={{
-          children: '2025-11-03',
-        }}
-      />
-      <Description
-        titleProps={{
-          widthType: 'narrow',
-          children: '訂單編號',
-        }}
-        contentProps={{
-          children: '#HXE3901270287719038',
-        }}
-      />
-      <Description
-        titleProps={{
-          widthType: 'narrow',
-          children: '訂單狀態',
-        }}
-        contentProps={{
-          variant: 'badge',
-          badge: {
-            variant: 'dot-success',
-            text: '已出貨',
-          },
-        }}
-      />
+      <Description title="訂購日期" widthType="narrow">
+        <DescriptionContent>2025-11-03</DescriptionContent>
+      </Description>
+      <Description title="訂單編號" widthType="narrow">
+        <DescriptionContent>#HXE3901270287719038</DescriptionContent>
+      </Description>
+      <Description title="訂單狀態" widthType="narrow">
+        <Badge variant="dot-success" text="已出貨" />
+      </Description>
     </DescriptionGroup>
   ),
 };
