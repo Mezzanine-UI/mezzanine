fd92e38361655d71aea1998545e5239623c62e30
BenChen <ans10528@gmail.com>
2026-03-09 12:52:05 +0800

    feat(core/tab): add size, add error status

--- packages/core/src/tab/tab.ts
+++ packages/core/src/tab/tab.ts
@@ -4,9 +4,12 @@ export const tabClasses = {
   host: tabPrefix,
   tabHorizontal: `${tabPrefix}--horizontal`,
   tabVertical: `${tabPrefix}--vertical`,
+  tabSizeMain: `${tabPrefix}--main`,
+  tabSizeSub: `${tabPrefix}--sub`,
   tabActiveBar: `${tabPrefix}__active-bar`,
   tabItem: `${tabPrefix}__item`,
   tabItemActive: `${tabPrefix}__item--active`,
+  tabItemError: `${tabPrefix}__item--error`,
   tabItemIcon: `${tabPrefix}__item__icon`,
   tabItemBadge: `${tabPrefix}__item__badge`,
 } as const;
